46eea80fc21e1cad483925077fbdc0b2f9522639
illyrius666 <fitimq@live.nl>
2024-10-22 15:12:30 +0000

    +

--- src/main/java/org/xodium/vanillaplus/commands/GUICommand.java
+++ src/main/java/org/xodium/vanillaplus/commands/GUICommand.java
@@ -10,15 +10,15 @@ public class GUICommand {
     public static void init(LifecycleEventManager<org.bukkit.plugin.Plugin> manager) {
         manager.registerEventHandler(LifecycleEvents.COMMANDS, e -> {
             e.registrar().register(
-                    Commands.literal("doorsplus")
+                    Commands.literal("vanillaplus")
                             .executes(ctx -> {
-                                ctx.getSource().getSender().sendMessage("Opening DoorsPlus GUI...");
+                                ctx.getSource().getSender().sendMessage("Opening VanillaPlus GUI...");
                                 // TODO: make this open the GUI
-                                ctx.getSource().getSender().sendMessage("DoorsPlus GUI opened!");
+                                ctx.getSource().getSender().sendMessage("VanillaPlus GUI opened!");
                                 return Command.SINGLE_SUCCESS;
                             })
                             .build(),
-                    "Opens the DoorsPlus GUI");
+                    "Opens the VanillaPlus GUI");
         });
     }
 }
