ebc7494d1c119ad693a5579ee000ec2220d02900
illyrius666 <fitimq@live.nl>
2024-10-26 10:18:21 +0000

    + refactored GUIManager

--- src/main/java/org/xodium/vanillaplus/managers/GUIManager.java
+++ src/main/java/org/xodium/vanillaplus/managers/GUIManager.java
@@ -18,77 +18,84 @@ import org.xodium.vanillaplus.interfaces.CONFIG;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 
-// TODO: add action to each setting (based on value) and save the value to the database.
-// TODO: fix size gui not working correctly.
-// TODO: make booleans show as True/False instead of 1/0.
+// TODO: implement saving of settings in db.
 public class GUIManager {
-    private final static Database db = new Database();
+    private static final Database db = new Database();
     public final Map<Player, Inventory> playerInventories = new HashMap<>();
     public final Map<Player, Integer> playerPageIndices = new HashMap<>();
     public final List<GUISettings> settings = new ArrayList<>();
-    {
+    private static final MiniMessage mm = MiniMessage.miniMessage();
+
+    public GUIManager() {
+        initializeSettings();
+    }
+
+    private void initializeSettings() {
         settings.add(
                 new GUISettings(Material.OAK_DOOR, "Knock on Wooden Door", List.of(CONFIG.SOUND_KNOCK_WOOD), (p) -> {
-                    // This is where the action should be performed
+                    // Action for knocking sound on wooden door
                 }));
         settings.add(new GUISettings(Material.BELL, "Set Knock Volume", List.of(CONFIG.SOUND_KNOCK_VOLUME), (p) -> {
-            // This is where the action should be performed
+            // Action for setting knock volume
         }));
         settings.add(new GUISettings(Material.NOTE_BLOCK, "Set Knock Pitch", List.of(CONFIG.SOUND_KNOCK_PITCH), (p) -> {
-            // This is where the action should be performed
+            // Action for setting knock pitch
         }));
         settings.add(
                 new GUISettings(Material.JUKEBOX, "Set Knock Category", List.of(CONFIG.SOUND_KNOCK_CATEGORY), (p) -> {
-                    // This is where the action should be performed
+                    // Action for setting knock category
                 }));
         settings.add(new GUISettings(Material.LEVER, "Allow Knocking", List.of(CONFIG.ALLOW_KNOCKING), (p) -> {
-            // This is where the action should be performed
+            // Action for allowing knocking
         }));
         settings.add(new GUISettings(Material.SPRUCE_TRAPDOOR, "Allow Knocking on Trapdoors",
                 List.of(CONFIG.ALLOW_KNOCKING_TRAPDOORS), (p) -> {
-                    // This is where the action should be performed
+                    // Action for knocking on trapdoors
                 }));
         settings.add(new GUISettings(Material.OAK_FENCE_GATE, "Allow Knocking on Gates",
                 List.of(CONFIG.ALLOW_KNOCKING_GATES), (p) -> {
-                    // This is where the action should be performed
+                    // Action for knocking on gates
                 }));
         settings.add(new GUISettings(Material.CLOCK, "Allow Auto-Close", List.of(CONFIG.ALLOW_AUTOCLOSE), (p) -> {
-            // This is where the action should be performed
+            // Action for auto-close
         }));
         settings.add(
                 new GUISettings(Material.RED_CARPET, "Require Shift to Knock", List.of(CONFIG.KNOCKING_REQUIRES_SHIFT),
                         (p) -> {
-                            // This is where the action should be performed
+                            // Action for requiring shift to knock
                         }));
         settings.add(new GUISettings(Material.BARRIER, "Require Empty Hand to Knock",
                 List.of(CONFIG.KNOCKING_REQUIRES_EMPTY_HAND), (p) -> {
-                    // This is where the action should be performed
+                    // Action for requiring empty hand to knock
                 }));
         settings.add(
                 new GUISettings(Material.SPRUCE_DOOR, "Allow Double Doors", List.of(CONFIG.ALLOW_DOUBLEDOORS), (p) -> {
-                    // This is where the action should be performed
+                    // Action for allowing double doors
                 }));
         settings.add(new GUISettings(Material.CLOCK, "Set Auto-Close Delay", List.of(CONFIG.AUTOCLOSE_DELAY), (p) -> {
-            // This is where the action should be performed
+            // Action for setting auto-close delay
         }));
     }
 
     public void openGUI(Player player) {
-        int settingsCount = settings.size();
-        int maxSlotsPerPage = 54;
-
         if (playerInventories.containsKey(player)) {
             player.closeInventory();
         }
 
         int currentPageIndex = playerPageIndices.getOrDefault(player, 0);
-        int startIdx = currentPageIndex * maxSlotsPerPage;
-        int inventorySize = maxSlotsPerPage;
-        MiniMessage mm = MiniMessage.miniMessage();
-        Inventory gui = Bukkit.createInventory(null, inventorySize,
+        Inventory gui = createGUI(player, currentPageIndex);
+
+        playerInventories.put(player, gui);
+        player.openInventory(gui);
+    }
+
+    private Inventory createGUI(Player player, int pageIndex) {
+        int maxSlotsPerPage = 54;
+        Inventory gui = Bukkit.createInventory(null, maxSlotsPerPage,
                 mm.deserialize("<b><gradient:#CB2D3E:#EF473A>VanillaPlus Settings</gradient></b>"));
 
-        for (int i = 0; i < maxSlotsPerPage && (startIdx + i) < settingsCount; i++) {
+        int startIdx = pageIndex * maxSlotsPerPage;
+        for (int i = 0; i < maxSlotsPerPage && (startIdx + i) < settings.size(); i++) {
             GUISettings setting = settings.get(startIdx + i);
             ItemStack item = new ItemStack(setting.getMaterial());
             ItemMeta meta = item.getItemMeta();
@@ -98,33 +105,44 @@ public class GUIManager {
             List<Component> loreComponents = new ArrayList<>();
             for (String key : setting.getLore()) {
                 Object value = db.getData(key);
-                loreComponents.add(mm.deserialize(value != null ? value.toString() : "N/A"));
+                loreComponents.add(mm.deserialize(convertToDisplayString(value)));
             }
-
             meta.lore(loreComponents);
             item.setItemMeta(meta);
             gui.setItem(i, item);
         }
 
-        if (settingsCount > maxSlotsPerPage) {
-            if (currentPageIndex > 0) {
-                ItemStack previousButton = new ItemStack(Material.ARROW);
-                ItemMeta previousMeta = previousButton.getItemMeta();
-                previousMeta.displayName(mm.deserialize("Previous Page"));
-                previousButton.setItemMeta(previousMeta);
-                gui.setItem(maxSlotsPerPage - 9, previousButton);
-            }
+        addPaginationButtons(gui, pageIndex);
+        return gui;
+    }
 
-            if (currentPageIndex < (int) Math.ceil(settingsCount / (double) maxSlotsPerPage) - 1) {
-                ItemStack nextButton = new ItemStack(Material.ARROW);
-                ItemMeta nextMeta = nextButton.getItemMeta();
-                nextMeta.displayName(mm.deserialize("Next Page"));
-                nextButton.setItemMeta(nextMeta);
-                gui.setItem(maxSlotsPerPage - 1, nextButton);
-            }
+    private void addPaginationButtons(Inventory gui, int pageIndex) {
+        int maxSlotsPerPage = 54;
+        int totalPageCount = (int) Math.ceil(settings.size() / (double) maxSlotsPerPage);
+
+        if (pageIndex > 0) {
+            ItemStack previousButton = createButton(Material.ARROW, "Previous Page");
+            gui.setItem(maxSlotsPerPage - 9, previousButton);
         }
 
-        playerInventories.put(player, gui);
-        player.openInventory(gui);
+        if (pageIndex < totalPageCount - 1) {
+            ItemStack nextButton = createButton(Material.ARROW, "Next Page");
+            gui.setItem(maxSlotsPerPage - 1, nextButton);
+        }
+    }
+
+    private ItemStack createButton(Material material, String displayName) {
+        ItemStack button = new ItemStack(material);
+        ItemMeta meta = button.getItemMeta();
+        meta.displayName(mm.deserialize(displayName));
+        button.setItemMeta(meta);
+        return button;
+    }
+
+    private String convertToDisplayString(Object value) {
+        if (value instanceof Boolean) {
+            return ((Boolean) value) ? "<b><green>True" : "<b><red>False";
+        }
+        return value != null ? value.toString() : "N/A";
     }
 }
