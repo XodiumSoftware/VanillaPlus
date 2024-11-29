81827fe29b1bd614e28ca1aed9b66cbf5ab9fb97
illyrius666 <fitimq@live.nl>
2024-11-29 16:23:59 +0000

    + implement MSG interface for consistent messaging and improve command handling

--- src/main/java/org/xodium/vanillaplus/commands/ReloadCommand.java
+++ src/main/java/org/xodium/vanillaplus/commands/ReloadCommand.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.bukkit.entity.Player;
 import org.xodium.vanillaplus.VanillaPlus;
+import org.xodium.vanillaplus.interfaces.MSG;
 import org.xodium.vanillaplus.interfaces.PERMS;
 import com.mojang.brigadier.Command;
 import io.papermc.paper.command.brigadier.Commands;
@@ -11,27 +12,28 @@ import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 
-public class ReloadCommand {
-    private final static VanillaPlus plugin = VanillaPlus.getInstance();
+public class ReloadCommand implements MSG {
+    private final static VanillaPlus vp = VanillaPlus.getInstance();
 
-    public static void init(LifecycleEventManager<org.bukkit.plugin.Plugin> manager) {
-        manager.registerEventHandler(LifecycleEvents.COMMANDS, e -> {
+    public static void init(LifecycleEventManager<org.bukkit.plugin.Plugin> man) {
+        man.registerEventHandler(LifecycleEvents.COMMANDS, e -> {
             e.registrar().register(
                     Commands.literal("vanillaplus")
                             .executes(ctx -> {
-                                if (!(ctx.getSource().getSender() instanceof Player player)) {
-                                    plugin.getLogger().warning("This command can only be executed by a player!");
+                                if (!(ctx.getSource().getSender() instanceof Player p)) {
+                                    vp.getLogger().warning("This command can only be executed by a player!");
                                     return 0;
                                 }
                                 MiniMessage mm = MiniMessage.miniMessage();
-                                if (!player.hasPermission(PERMS.RELOAD)) {
-                                    player.sendMessage(
-                                            mm.deserialize("<red>You do not have permission to use this command!"));
+                                if (!p.hasPermission(PERMS.RELOAD)) {
+                                    p.sendMessage(
+                                            mm.deserialize(
+                                                    PREFIX + "<red>You do not have permission to use this command!"));
                                     return 0;
                                 }
-                                plugin.reloadConfig();
-                                player.sendMessage(mm.deserialize("<green>Configuration reloaded successfully."));
-                                plugin.getLogger().info("Configuration reloaded successfully.");
+                                vp.reloadConfig();
+                                p.sendMessage(mm.deserialize(PREFIX + "<green>Configuration reloaded successfully."));
+                                vp.getLogger().info("Configuration reloaded successfully.");
                                 return Command.SINGLE_SUCCESS;
                             })
                             .build(),
--- src/main/java/org/xodium/vanillaplus/modules/ElevatorPlusModule.java
+++ src/main/java/org/xodium/vanillaplus/modules/ElevatorPlusModule.java
@@ -6,24 +6,30 @@ import java.util.Map;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.util.Vector;
+import org.xodium.vanillaplus.interfaces.MSG;
 
-public class ElevatorPlusModule implements Listener {
+import net.kyori.adventure.text.minimessage.MiniMessage;
+
+public class ElevatorPlusModule implements Listener, MSG {
     private final Map<Vector, Vector> elevators = new HashMap<>();
+    private final MiniMessage mm = MiniMessage.miniMessage();
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent e) {
         Block b = e.getBlock();
+        Player p = e.getPlayer();
         if (b.getType() == Material.HEAVY_WEIGHTED_PRESSURE_PLATE) {
             Block below = b.getRelative(BlockFace.DOWN);
             if (below.getType() == Material.NOTE_BLOCK) {
                 Vector loc = b.getLocation().toVector();
                 elevators.put(loc, loc);
-                e.getPlayer().sendMessage("Elevator created at " + loc);
+                p.sendMessage(mm.deserialize(PREFIX + "<aqua>Elevator created at: (" + loc + ")"));
             }
         }
     }
@@ -31,22 +37,23 @@ public class ElevatorPlusModule implements Listener {
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent e) {
         Block b = e.getTo().getBlock();
+        Player p = e.getPlayer();
         if (b != null && b.getType() == Material.HEAVY_WEIGHTED_PRESSURE_PLATE) {
             Vector loc = b.getLocation().toVector();
             if (elevators.containsKey(loc)) {
-                if (e.getPlayer().isSneaking()) {
+                if (p.isSneaking()) {
                     Vector dest = findDestination(loc, false);
                     if (dest != null) {
-                        e.getPlayer().teleport(dest.toLocation(b.getWorld()).add(0.5, 1, 0.5));
+                        p.teleport(dest.toLocation(b.getWorld()).add(0.5, 1, 0.5));
                     } else {
-                        e.getPlayer().sendMessage("No corresponding elevator found below.");
+                        p.sendMessage(mm.deserialize(PREFIX + "<yellow>No corresponding elevator found below."));
                     }
-                } else if (e.getPlayer().getVelocity().getY() > 0) {
+                } else if (p.getVelocity().getY() > 0) {
                     Vector dest = findDestination(loc, true);
                     if (dest != null) {
-                        e.getPlayer().teleport(dest.toLocation(b.getWorld()).add(0.5, 1, 0.5));
+                        p.teleport(dest.toLocation(b.getWorld()).add(0.5, 1, 0.5));
                     } else {
-                        e.getPlayer().sendMessage("No corresponding elevator found above.");
+                        p.sendMessage(mm.deserialize(PREFIX + "<yellow>No corresponding elevator found above."));
                     }
                 }
             }
