3cc3b9b22e95c0727b2e6d235f6ee4349e149cc5
illyrius666 <fitimq@live.nl>
2024-11-29 17:54:35 +0000

    + ElevatorPlusModule to enhance teleportation logic and remove unused elevator persistence

--- src/main/java/org/xodium/vanillaplus/modules/ElevatorPlusModule.java
+++ src/main/java/org/xodium/vanillaplus/modules/ElevatorPlusModule.java
@@ -1,35 +1,26 @@
 package org.xodium.vanillaplus.modules;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
-
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.util.Vector;
-import org.xodium.vanillaplus.VanillaPlus;
 import org.xodium.vanillaplus.interfaces.MSG;
 
 import net.kyori.adventure.text.minimessage.MiniMessage;
 
 public class ElevatorPlusModule implements Listener, MSG {
-    private final VanillaPlus vp = VanillaPlus.getInstance();
     private final Map<Vector, Vector> elevators = new HashMap<>();
     private final MiniMessage mm = MiniMessage.miniMessage();
-
-    {
-        loadElevators();
-    }
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent e) {
@@ -39,21 +30,24 @@ public class ElevatorPlusModule implements Listener, MSG {
             Vector loc = b.getLocation().toVector();
             elevators.put(loc, loc);
             e.getPlayer().sendMessage(mm.deserialize(PREFIX + "<aqua>Elevator created at: (" + loc + ")"));
-            saveElevators();
         }
     }
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent e) {
         Block b = e.getTo().getBlock();
-        Player p = e.getPlayer();
         if (b != null && b.getType() == Material.HEAVY_WEIGHTED_PRESSURE_PLATE) {
             Vector loc = b.getLocation().toVector();
             if (elevators.containsKey(loc)) {
-                boolean goingUp = !p.isSneaking() && p.getVelocity().getY() > 0;
-                Vector dest = findDestination(loc, goingUp);
+                Player p = e.getPlayer();
+                Vector dest = findDestination(loc, !p.isSneaking() && p.getVelocity().getY() > 0);
                 if (dest != null) {
-                    p.teleport(dest.toLocation(b.getWorld()).add(0.5, 1, 0.5));
+                    Location viewloc = dest.toLocation(b.getWorld()).add(0.5, 1, 0.5);
+                    viewloc.setPitch(p.getLocation().getPitch());
+                    viewloc.setYaw(p.getLocation().getYaw());
+                    p.teleport(viewloc);
+                    p.playSound(viewloc, Sound.ENTITY_ENDERMAN_TELEPORT, 1.0f, 1.0f);
+                    b.getWorld().spawnParticle(Particle.PORTAL, viewloc, 50, 0.5, 1, 0.5, 0.1);
                 }
             }
         }
@@ -66,31 +60,5 @@ public class ElevatorPlusModule implements Listener, MSG {
                 .filter(loc -> goingUp ? loc.getY() > currentLoc.getY() : loc.getY() < currentLoc.getY())
                 .findFirst()
                 .orElse(null);
-    }
-
-    // TODO: make database instead.
-    private void saveElevators() {
-        File file = new File(vp.getDataFolder(), "elevators.yml");
-        FileConfiguration config = YamlConfiguration.loadConfiguration(file);
-        config.set("elevators", elevators.keySet().stream().map(Vector::toString).collect(Collectors.toList()));
-        try {
-            config.save(file);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void loadElevators() {
-        File file = new File(vp.getDataFolder(), "elevators.yml");
-        if (!file.exists()) {
-            return;
-        }
-        FileConfiguration config = YamlConfiguration.loadConfiguration(file);
-        config.getStringList("elevators").forEach(loc -> {
-            String[] parts = loc.replace("(", "").replace(")", "").split(",");
-            Vector vector = new Vector(Double.parseDouble(parts[0]), Double.parseDouble(parts[1]),
-                    Double.parseDouble(parts[2]));
-            elevators.put(vector, vector);
-        });
     }
 }
