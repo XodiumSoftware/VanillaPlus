4a265eab39cf0c95a7c6ae03ba4786b9bb27610d
illyrius666 <fitimq@live.nl>
2024-12-04 13:35:27 +0000

    + Move plugin.yml content from build.gradle.kts to a dedicated src/main/resources/plugin.yml file for better organization and maintainability

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,38 +37,4 @@ tasks {
             println(project.version)
         }
     }
-    register<Jar>("customJar") {
-        dependsOn("jar")
-        from(sourceSets.main.get().output)
-        doFirst {
-            val pluginYmlContent = """
-                name: VanillaPlus
-                description: ${project.description}
-                main: org.xodium.vanillaplus.VanillaPlus
-                version: ${project.version}
-                api-version: 1.21.3
-                authors:
-                  - XodiumSoftware
-                commands:
-                  vanillaplus:
-                    description: Main command for VanillaPlus
-                    usage: /vanillaplus
-                permissions:
-                  vanillaplus.doubledoors:
-                    description: Allows to open double doors with one click
-                    default: true
-                  vanillaplus.knock:
-                    description: Allows to knock on doors using left-click
-                    default: true
-                  vanillaplus.autoclose:
-                    description: Allows players to have their doors close automatically
-                    default: true
-            """.trimIndent()
-            val pluginYmlFile = file("$buildDir/tmp/plugin.yml")
-            pluginYmlFile.writeText(pluginYmlContent)
-        }
-        from("$buildDir/tmp/plugin.yml") {
-            into("META-INF")
-        }
-    }
 }
