71a7c44bc50d7bfeb3425d9b40f4e678e986067b
illyrius666 <fitimq@live.nl>
2024-12-06 08:30:41 +0000

    +

--- src/main/java/org/xodium/vanillaplus/ModuleManager.java
+++ src/main/java/org/xodium/vanillaplus/ModuleManager.java
@@ -1,23 +1,19 @@
 package org.xodium.vanillaplus;
 
 import org.xodium.vanillaplus.interfaces.MSG;
+import org.xodium.vanillaplus.interfaces.Modular;
 import org.xodium.vanillaplus.modules.DoorsModule;
 
 import java.util.List;
 
-// TODO: make the config actually work.
 public class ModuleManager {
     private final VanillaPlus vp = VanillaPlus.getInstance();
 
     {
         List.of(new DoorsModule())
                 .stream()
-                .peek(module -> {
-                    module.config();
-                })
-                .filter(module -> {
-                    return module.isEnabled();
-                })
+                .peek(Modular::config)
+                .filter(Modular::isEnabled)
                 .forEach(module -> {
                     vp.getServer().getPluginManager().registerEvents(module, vp);
                     vp.getLogger().info(MSG.MODULE_LOADED + module.getClass().getSimpleName());
