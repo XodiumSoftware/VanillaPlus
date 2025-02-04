9169baa6d37adee991a2661b2db2eb46f889da88
illyrius666 <fitimq@live.nl>
2025-02-04 18:51:25 +0100

    some small fixes

--- src/main/kotlin/org/xodium/vanillaplus/modules/TreesModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/TreesModule.kt
@@ -12,7 +12,6 @@ import com.sk89q.worldedit.function.mask.BlockTypeMask
 import com.sk89q.worldedit.function.operation.Operations
 import com.sk89q.worldedit.math.BlockVector3
 import com.sk89q.worldedit.session.ClipboardHolder
-import com.sk89q.worldedit.world.block.BlockTypes
 import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.block.Block
@@ -23,6 +22,7 @@ import org.xodium.vanillaplus.Config
 import org.xodium.vanillaplus.Utils
 import org.xodium.vanillaplus.VanillaPlus.Companion.instance
 import org.xodium.vanillaplus.interfaces.ModuleInterface
+import org.xodium.vanillaplus.registries.TreeRegistry
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -30,90 +30,12 @@ import java.nio.file.Paths
 
 class TreesModule : ModuleInterface {
     private val logger = instance.logger
-    private val schematicsPath = instance.dataFolder.toPath().resolve(SCHEMATICS_FOLDER)
+    private val schematicsFolder = Paths.get("schematics")
+    private val schematicsPath = instance.dataFolder.toPath().resolve(schematicsFolder)
     private lateinit var saplingSchematicMap: Map<Material, List<Path>>
 
-    companion object {
-        private val SCHEMATICS_FOLDER = Paths.get("schematics")
-
-        private val SAPLINGS = setOf(
-            Material.ACACIA_SAPLING,
-            Material.BIRCH_SAPLING,
-            Material.CHERRY_SAPLING,
-            Material.DARK_OAK_SAPLING,
-            Material.JUNGLE_SAPLING,
-            Material.MANGROVE_PROPAGULE,
-            Material.OAK_SAPLING,
-            Material.PALE_OAK_SAPLING,
-            Material.SPRUCE_SAPLING,
-        )
-        private val OVERRIDEABLE_BLOCKS = setOf(
-            // General
-            BlockTypes.AIR,
-            // Greenery
-            BlockTypes.SHORT_GRASS,
-            BlockTypes.TALL_GRASS,
-            BlockTypes.SHORT_GRASS,
-            BlockTypes.FERN,
-            BlockTypes.LARGE_FERN,
-            BlockTypes.DEAD_BUSH,
-            BlockTypes.VINE,
-            BlockTypes.SEAGRASS,
-            BlockTypes.TALL_SEAGRASS,
-            BlockTypes.SUGAR_CANE,
-            BlockTypes.KELP,
-            BlockTypes.KELP_PLANT,
-            BlockTypes.CAVE_VINES,
-            BlockTypes.CAVE_VINES_PLANT,
-            BlockTypes.WEEPING_VINES,
-            BlockTypes.WEEPING_VINES_PLANT,
-            BlockTypes.TWISTING_VINES,
-            BlockTypes.TWISTING_VINES_PLANT,
-            BlockTypes.FLOWERING_AZALEA_LEAVES,
-            BlockTypes.AZALEA_LEAVES,
-            BlockTypes.AZALEA,
-            BlockTypes.FLOWERING_AZALEA,
-            BlockTypes.SNOW,
-            BlockTypes.MOSS_CARPET,
-            BlockTypes.MOSS_BLOCK,
-            // Small Flowers
-            BlockTypes.ALLIUM,
-            BlockTypes.AZURE_BLUET,
-            BlockTypes.BLUE_ORCHID,
-            BlockTypes.CORNFLOWER,
-            BlockTypes.DANDELION,
-            BlockTypes.CLOSED_EYEBLOSSOM,
-            BlockTypes.OPEN_EYEBLOSSOM,
-            BlockTypes.LILY_OF_THE_VALLEY,
-            BlockTypes.OXEYE_DAISY,
-            BlockTypes.POPPY,
-            BlockTypes.TORCHFLOWER,
-            BlockTypes.ORANGE_TULIP,
-            BlockTypes.PINK_TULIP,
-            BlockTypes.RED_TULIP,
-            BlockTypes.WHITE_TULIP,
-            BlockTypes.WITHER_ROSE,
-            // Tall Flowers
-            BlockTypes.LILAC,
-            BlockTypes.PEONY,
-            BlockTypes.PITCHER_PLANT,
-            BlockTypes.ROSE_BUSH,
-            BlockTypes.SUNFLOWER,
-            // Other Flowers
-            BlockTypes.CHERRY_LEAVES,
-            BlockTypes.CHORUS_FLOWER,
-            BlockTypes.FLOWERING_AZALEA,
-            BlockTypes.FLOWERING_AZALEA_LEAVES,
-            BlockTypes.MANGROVE_PROPAGULE,
-            BlockTypes.PINK_PETALS,
-//            TODO("Add in 1.21.5"),
-//            BlockTypes.WILDFLOWERS,
-            BlockTypes.SPORE_BLOSSOM,
-        )
-    }
-
     override fun init() {
-        Utils.copyResourcesFromJar(SCHEMATICS_FOLDER, schematicsPath)
+        Utils.copyResourcesFromJar(schematicsFolder, schematicsPath)
         saplingSchematicMap = loadSaplingSchematicMap()
     }
 
@@ -125,7 +47,7 @@ class TreesModule : ModuleInterface {
 
     private fun validateAndMapSapling(key: String, value: Any?): Pair<Material, List<Path>>? {
         val material = Material.matchMaterial(key)
-        if (material == null || !SAPLINGS.contains(material)) {
+        if (material == null || !TreeRegistry.SAPLINGS.contains(material)) {
             logger.warning("Invalid sapling configuration entry: $key does not map to a valid sapling.")
             return null
         }
@@ -139,7 +61,7 @@ class TreesModule : ModuleInterface {
 
     @EventHandler(priority = EventPriority.MONITOR)
     fun on(event: StructureGrowEvent) {
-        event.location.block.takeIf { SAPLINGS.contains(it.type) }?.let {
+        event.location.block.takeIf { TreeRegistry.SAPLINGS.contains(it.type) }?.let {
             event.isCancelled = replaceWithSchematicTree(it)
         }
     }
@@ -163,7 +85,7 @@ class TreesModule : ModuleInterface {
                         WorldEdit.getInstance().newEditSession(BukkitAdapter.adapt(block.world))
                             .use { editSession ->
                                 block.type = Material.AIR
-                                editSession.mask = BlockTypeMask(editSession, OVERRIDEABLE_BLOCKS)
+                                editSession.mask = BlockTypeMask(editSession, TreeRegistry.OVERRIDEABLE_BLOCKS)
                                 Operations.complete(
                                     ClipboardHolder(clipboard)
                                         .createPaste(editSession)
