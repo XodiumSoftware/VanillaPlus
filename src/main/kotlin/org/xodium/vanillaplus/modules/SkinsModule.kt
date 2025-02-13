f0f0c1edf0379351c247df20b5490ee86b28f9e1
Illyrius <FitimQ@live.nl>
2025-02-13 15:16:17 +0100

    woops

--- src/main/kotlin/org/xodium/vanillaplus/modules/SkinsModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/SkinsModule.kt
@@ -84,7 +84,7 @@ class SkinsModule : ModuleInterface {
 
     private fun toggleSkin(player: Player, skinData: SkinData) {
         if (!isUnlocked(player, skinData)) {
-            player.sendMessage("<red>Locked! Defeat the ${skinData.entityName} to unlock this skin.".mm())
+            player.sendMessage("${VanillaPlus.PREFIX}<red>Locked! Defeat the ${skinData.entityName} to unlock this skin.".mm())
             return
         }
         val heldItem = player.inventory.getItem(player.inventory.heldItemSlot) ?: return
