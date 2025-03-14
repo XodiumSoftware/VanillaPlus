3797707e6e9f56f8fc66154c5fa4fdb7b254674c
illyrius666 <fitimq@live.nl>
2025-03-14 21:19:33 +0100

    finished vp cmd

--- src/main/kotlin/org/xodium/vanillaplus/managers/CommandManager.kt
+++ src/main/kotlin/org/xodium/vanillaplus/managers/CommandManager.kt
@@ -10,12 +10,13 @@ package org.xodium.vanillaplus.managers
 import com.mojang.brigadier.Command
 import io.papermc.paper.command.brigadier.Commands
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
+import net.kyori.adventure.text.event.ClickEvent
 import org.bukkit.entity.Player
 import org.xodium.vanillaplus.Perms
 import org.xodium.vanillaplus.Utils
+import org.xodium.vanillaplus.Utils.mm
+import org.xodium.vanillaplus.VanillaPlus.Companion.PREFIX
 import org.xodium.vanillaplus.VanillaPlus.Companion.instance
-import org.xodium.vanillaplus.modules.AutoRefillModule
-import org.xodium.vanillaplus.modules.AutoToolModule
 
 /**
  * Class for registering GUI commands.
@@ -26,15 +27,22 @@ object CommandManager {
             event.registrar().register(
                 Commands.literal(instance.name.lowercase())
                     .requires { it.sender.hasPermission(Perms.Use.GENERAL) }
-                    .executes(Command { Utils.tryCatch(it) { TODO() } }
-                    ).then(
-                        Commands.literal("autotool")
-                            .requires { it.sender.hasPermission(Perms.AutoTool.USE) }
-                            .executes(Command { Utils.tryCatch(it) { AutoToolModule().toggle(it.sender as Player) } })
-                    ).then(
-                        Commands.literal("autorefill")
-                            .requires { it.sender.hasPermission(Perms.AutoRefill.USE) }
-                            .executes(Command { Utils.tryCatch(it) { AutoRefillModule().toggle(it.sender as Player) } })
+                    .executes(Command {
+                        Utils.tryCatch(it) {
+                            (it.sender as Player).sendMessage(
+                                "$PREFIX v${instance.pluginMeta.version} | Click on me for more info!".mm()
+                                    .clickEvent(ClickEvent.suggestCommand("/help ${instance.name.lowercase()}"))
+                            )
+                        }
+                    }
+//                    ).then(
+//                        Commands.literal("autotool")
+//                            .requires { it.sender.hasPermission(Perms.AutoTool.USE) }
+//                            .executes(Command { Utils.tryCatch(it) { AutoToolModule().toggle(it.sender as Player) } })
+//                    ).then(
+//                        Commands.literal("autorefill")
+//                            .requires { it.sender.hasPermission(Perms.AutoRefill.USE) }
+//                            .executes(Command { Utils.tryCatch(it) { AutoRefillModule().toggle(it.sender as Player) } })
                     )
                     .build(),
                 "${instance.name} plugin",
