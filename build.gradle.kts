12a800c588c3b3f539af6b8c77dc0ac5a30cd9b8
Illyrius <FitimQ@live.nl>
2025-03-19 11:21:31 +0100

    v+

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 group = "org.xodium.vanillaplus"
-version = "1.4.0"
+version = "1.5.0"
 description = "Minecraft plugin that enhances the base gameplay."
 
 var pluginName: String = "VanillaPlus"
