c09dd0012cd1f1d11d4b528a47973b6b21453b8d
illyrius666 <fitimq@live.nl>
2025-04-30 14:33:07 +0200

    boop
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/modules/WaystoneModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/WaystoneModule.kt
@@ -42,14 +42,11 @@ import org.xodium.vanillaplus.utils.Utils
 import java.util.*
 
 //TODO: Add that you have to discover waypoints manually first before being able to use them.
-//TODO: Adjust effects to be around the waystone instead of player.
-//TODO: Add waystone custom texture.
 
 /**
  * Represents a module handling waystone mechanics within the system.
  * A waystone allows players to teleport between locations using specific in-game constructs.
  * The module listens for relevant player and block events and manages waystone functionality accordingly.
- *
  * Features:
  * - Automatically enables if the module is configured as enabled.
  * - Initializes the required database table and crafting recipe on enabling.
@@ -123,12 +120,11 @@ class WaystoneModule : ModuleInterface {
 
     /**
      * Creates an initial visual and auditory effect at the specified location, simulating the start of a teleportation process.
-     *
      * @param location The location where the teleportation initiation effects (particles and sound) will be generated.
      */
     private fun teleportInitEffect(location: Location) {
-        location.world.spawnParticle(Particle.PORTAL, location.add(0.0, 1.0, 0.0), 20, 0.5, 1.0, 0.5, 0.1)
-        location.world.playSound(location, Sound.BLOCK_NOTE_BLOCK_HAT, 0.2f, 2.0f)
+        location.world.spawnParticle(Particle.PORTAL, location.add(0.0, 1.0, 0.0), 20, 0.5, 1.0, 0.5, 0.2)
+        location.world.playSound(location, Sound.BLOCK_BEACON_ACTIVATE, 0.2f, 2.0f)
     }
 
     /**
@@ -143,7 +139,6 @@ class WaystoneModule : ModuleInterface {
 
     /**
      * Creates a visual and auditory effect at the specified location, simulating the activation of a waystone.
-     *
      * @param location The location where the waystone activation effects (particles and sound) will be generated.
      */
     private fun waystoneCreateEffect(location: Location) {
@@ -160,7 +155,6 @@ class WaystoneModule : ModuleInterface {
 
     /**
      * Creates visual and auditory effects at the specified location, simulating the deletion of a waystone.
-     *
      * @param location The location where the waystone deletion effects (particles and sound) will be generated.
      */
     private fun waystoneDeleteEffect(location: Location) {
@@ -185,10 +179,8 @@ class WaystoneModule : ModuleInterface {
      * @param targetWaystone The target waystone data containing the destination location for teleportation.
      */
     private fun handleTeleportation(player: Player, targetWaystone: WaystoneData) {
-        val originLoc = originWaystone[player.uniqueId]
-        val xpCost = calculateXpCost(
-            originLoc ?: player.location, targetWaystone.location, player.isInsideVehicle
-        )
+        val originLoc = originWaystone[player.uniqueId] ?: return
+        val xpCost = calculateXpCost(originLoc, targetWaystone.location, player.isInsideVehicle)
 
         if (player.gameMode in listOf(GameMode.SURVIVAL, GameMode.ADVENTURE)) {
             val playerTotalXp = player.level * 7 + player.exp.toInt()
@@ -227,7 +219,9 @@ class WaystoneModule : ModuleInterface {
                     if (player.gameMode in listOf(GameMode.SURVIVAL, GameMode.ADVENTURE)) {
                         Utils.chargePlayerXp(player, xpCost)
                     }
+
                     teleportEffect(player.location)
+
                     val targetLocation = getTeleportLocationNextTo(targetWaystone.location, mountedEntity != null)
                     if (mountedEntity != null) {
                         mountedEntity.teleportAsync(
@@ -238,6 +232,7 @@ class WaystoneModule : ModuleInterface {
                     } else {
                         player.teleportAsync(targetLocation)
                     }
+
                     teleportEffect(targetLocation)
                     return@runTaskTimer task.cancel()
                 } else {
@@ -365,10 +360,8 @@ class WaystoneModule : ModuleInterface {
 
     /**
      * Creates a graphical user interface (GUI) for the player, allowing them to interact with and teleport to waystones.
-     *
      * The GUI is dynamically generated based on the filtered list of available waystones, excludes the player's current waystone
      * (if any), and displays relevant information such as the waystone's name and teleportation cost.
-     *
      * @param player The player for whom the GUI is being generated.
      * @return A `Gui` instance representing the graphical user interface for waystone interaction.
      */
