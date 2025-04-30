e7a0e4e4b59f822d2984fea627783867c3fa2450
illyrius666 <fitimq@live.nl>
2025-04-30 15:46:26 +0200

    Add utility functions for CustomModelData and ItemLore in ExtUtils
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/utils/ExtUtils.kt
+++ src/main/kotlin/org/xodium/vanillaplus/utils/ExtUtils.kt
@@ -11,6 +11,7 @@ import io.papermc.paper.datacomponent.item.CustomModelData
 import io.papermc.paper.datacomponent.item.ItemLore
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.Color
 
 /** Extension utilities */
 object ExtUtils {
@@ -24,15 +25,45 @@ object ExtUtils {
     /** Deserializes a list of MiniMessage strings into a list of Components. */
     fun List<String>.mm(): List<Component> = this.map { it.mm() }
 
+    /** Serializes a Component into a MiniMessage string. */
+    fun Component.mm(): String = MM.serialize(this)
+
+    /** Deserializes a list of MiniMessage strings into a list of Components. */
+    fun List<Component>.mm(): List<String> = this.map { it.mm() }
+
     /** Creates an ItemLore object from a single MiniMessage string. */
     fun String.il(): ItemLore.Builder = IL.addLine(this.mm())
 
     /** Creates an ItemLore object from a list of MiniMessage strings. */
     fun List<String>.il(): ItemLore.Builder = IL.addLines(this.mm())
 
+    /** Creates an ItemLore object from a single Component. */
+    fun Component.il(): ItemLore.Builder = IL.addLine(this)
+
+    /** Creates an ItemLore object from a list of Components. */
+    fun List<Component>.il(): ItemLore.Builder = IL.addLines(this)
+
     /** Creates a CustomModelData object from a single string. */
     fun String.cmd(): CustomModelData.Builder = CMD.addString(this)
 
     /** Creates a CustomModelData object from a list of strings. */
     fun List<String>.cmd(): CustomModelData.Builder = CMD.addStrings(this)
+
+    /** Creates a CustomModelData object from a single float. */
+    fun Float.cmd(): CustomModelData.Builder = CMD.addFloat(this)
+
+    /** Creates a CustomModelData object from a list of floats. */
+    fun List<Float>.cmd(): CustomModelData.Builder = CMD.addFloats(this)
+
+    /** Creates a CustomModelData object from a single boolean. */
+    fun Boolean.cmd(): CustomModelData.Builder = CMD.addFlag(this)
+
+    /** Creates a CustomModelData object from a list of booleans. */
+    fun List<Boolean>.cmd(): CustomModelData.Builder = CMD.addFlags(this)
+
+    /** Creates a CustomModelData object from a single color. */
+    fun Color.cmd(): CustomModelData.Builder = CMD.addColor(this)
+
+    /** Creates a CustomModelData object from a list of colors. */
+    fun List<Color>.cmd(): CustomModelData.Builder = CMD.addColors(this)
 }
