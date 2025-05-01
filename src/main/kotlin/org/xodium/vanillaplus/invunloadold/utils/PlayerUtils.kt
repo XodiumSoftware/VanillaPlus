cf5f8db986b62f487472fb9a2521f92ea568db7a
illyrius666 <fitimq@live.nl>
2025-05-01 20:04:59 +0200

    refactor: enhance PlayerUtils to manage chest access and improve interaction handling
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/invunloadold/utils/PlayerUtils.kt
+++ src/main/kotlin/org/xodium/vanillaplus/invunloadold/utils/PlayerUtils.kt
@@ -6,24 +6,41 @@
 package org.xodium.vanillaplus.invunloadold.utils
 
 import org.bukkit.block.Block
-import org.bukkit.block.BlockFace
 import org.bukkit.entity.Player
-import org.bukkit.event.Event
+import org.bukkit.event.EventHandler
 import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
-import org.xodium.vanillaplus.VanillaPlus.Companion.instance
-import org.xodium.vanillaplus.invunloadold.api.InvUnloadCheckAccessEvent
+import java.util.*
 
 object PlayerUtils {
-    fun canPlayerUseChest(block: Block?, player: Player?): Boolean {
-        if (instance.config.getBoolean("use-playerinteractevent")) { //TODO: use Config
-            val event: PlayerInteractEvent = InvUnloadCheckAccessEvent(
-                player!!,
-                Action.RIGHT_CLICK_BLOCK, null, block, BlockFace.UP
-            )
-            instance.server.pluginManager.callEvent(event)
-            if (event.useInteractedBlock() == Event.Result.DENY) return false
+    private val deniedAccess: MutableSet<Pair<UUID, Block>> = Collections.synchronizedSet(mutableSetOf())
+
+    @EventHandler
+    fun onPlayerInteract(event: PlayerInteractEvent) {
+        if (event.action == Action.RIGHT_CLICK_BLOCK && event.clickedBlock != null) {
+            if (event.clickedBlock!!.type.name.contains("CHEST")
+            ) {
+                deny(event.player, event.clickedBlock!!)
+            } else {
+                clear(event.player, event.clickedBlock!!)
+            }
         }
-        return true
+    }
+
+    fun canPlayerUseChest(block: Block?, player: Player?): Boolean {
+        if (block == null || player == null) return false
+        return !isDenied(player, block)
+    }
+
+    private fun deny(player: Player, block: Block) {
+        deniedAccess.add(player.uniqueId to block)
+    }
+
+    private fun isDenied(player: Player, block: Block): Boolean {
+        return deniedAccess.contains(player.uniqueId to block)
+    }
+
+    private fun clear(player: Player, block: Block) {
+        deniedAccess.remove(player.uniqueId to block)
     }
 }
