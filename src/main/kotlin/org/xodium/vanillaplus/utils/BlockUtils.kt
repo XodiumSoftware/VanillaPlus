c1b6809940745796929b04a7d4ce9f1385702d1a
illyrius666 <fitimq@live.nl>
2025-05-21 15:58:28 +0200

    feat: enhance BlockUtils with additional rotation methods for BlockFace
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/utils/BlockUtils.kt
+++ src/main/kotlin/org/xodium/vanillaplus/utils/BlockUtils.kt
@@ -3,18 +3,50 @@
  *  All rights reserved.
  */
 
+@file:Suppress("unused")
+
 package org.xodium.vanillaplus.utils
 
 import org.bukkit.block.BlockFace
 
 /** Block utilities. */
 object BlockUtils {
-    /** Rotates the given [BlockFace] 90 degrees clockwise. */
-    fun BlockFace.rotateY(): BlockFace = when (this) {
-        BlockFace.NORTH -> BlockFace.EAST
-        BlockFace.EAST -> BlockFace.SOUTH
-        BlockFace.SOUTH -> BlockFace.WEST
-        BlockFace.WEST -> BlockFace.NORTH
+    /**
+     * Rotates the given [BlockFace] 90 degrees clockwise around the Y-axis.
+     * @param clockwise If true, rotates clockwise; otherwise, rotates counter-clockwise.
+     * @return The rotated [BlockFace].
+     */
+    fun BlockFace.rotateY(clockwise: Boolean = true): BlockFace = when (this) {
+        BlockFace.NORTH -> if (clockwise) BlockFace.EAST else BlockFace.WEST
+        BlockFace.EAST -> if (clockwise) BlockFace.SOUTH else BlockFace.NORTH
+        BlockFace.SOUTH -> if (clockwise) BlockFace.WEST else BlockFace.EAST
+        BlockFace.WEST -> if (clockwise) BlockFace.NORTH else BlockFace.SOUTH
+        else -> this
+    }
+
+    /**
+     * Rotates the given [BlockFace] 90 degrees clockwise around the X-axis.
+     * @param clockwise If true, rotates clockwise; otherwise, rotates counter-clockwise.
+     * @return The rotated [BlockFace].
+     */
+    fun BlockFace.rotateX(clockwise: Boolean = true): BlockFace = when (this) {
+        BlockFace.UP -> if (clockwise) BlockFace.NORTH else BlockFace.SOUTH
+        BlockFace.NORTH -> if (clockwise) BlockFace.DOWN else BlockFace.UP
+        BlockFace.DOWN -> if (clockwise) BlockFace.SOUTH else BlockFace.NORTH
+        BlockFace.SOUTH -> if (clockwise) BlockFace.UP else BlockFace.DOWN
+        else -> this
+    }
+
+    /**
+     * Rotates the given [BlockFace] 90 degrees clockwise around the Z-axis.
+     * @param clockwise If true, rotates clockwise; otherwise, rotates counter-clockwise.
+     * @return The rotated [BlockFace].
+     */
+    fun BlockFace.rotateZ(clockwise: Boolean = true): BlockFace = when (this) {
+        BlockFace.UP -> if (clockwise) BlockFace.EAST else BlockFace.WEST
+        BlockFace.EAST -> if (clockwise) BlockFace.DOWN else BlockFace.UP
+        BlockFace.DOWN -> if (clockwise) BlockFace.WEST else BlockFace.EAST
+        BlockFace.WEST -> if (clockwise) BlockFace.UP else BlockFace.DOWN
         else -> this
     }
 }
