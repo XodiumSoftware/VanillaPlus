54e71aebe425ae1e98c09ebddf3c266be77c7883
illyrius666 <fitimq@live.nl>
2025-05-27 13:15:36 +0200

    cleanup
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/Config.kt
+++ src/main/kotlin/org/xodium/vanillaplus/Config.kt
@@ -197,14 +197,6 @@ object Config {
     object ChestSortModule {
         /** Enables or disables the ChestSortModule. */
         var ENABLED: Boolean = true
-
-        /** The sound effect used for sorting chests. */
-        var SOUND_ON_SORT: Sound = Sound.sound(
-            BukkitSound.ENTITY_PLAYER_LEVELUP,
-            Sound.Source.PLAYER,
-            1.0f,
-            1.0f
-        )
     }
 
     /** Configuration settings for the ChiselModule. */
