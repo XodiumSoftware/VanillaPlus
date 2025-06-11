c8ddca90dbdc997d7d31c6fe7552861bedf10785
illyrius666 <fitimq@live.nl>
2025-06-11 14:50:23 +0200

    refactor: simplify version and platform checks in VanillaPlus class
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/VanillaPlus.kt
+++ src/main/kotlin/org/xodium/vanillaplus/VanillaPlus.kt
@@ -14,12 +14,12 @@ import org.xodium.vanillaplus.utils.FmtUtils.mangoFmt
 /** Main class of the plugin. */
 class VanillaPlus : JavaPlugin() {
     companion object {
-        private val SUPPORTED_VERSIONS = setOf("1.21.5")
-        private val SUPPORTED_PLATFORMS = setOf("Paper")
-        private val UNSUPPORTED_PLATFORM_MSG =
-            "This plugin requires a supported server platform. Supported platforms: ${SUPPORTED_PLATFORMS.joinToString(", ")}."
-        private val UNSUPPORTED_VERSION_MSG =
-            "This plugin requires a supported server version. Supported versions: ${SUPPORTED_VERSIONS.joinToString(", ")}."
+        private const val SUPPORTED_VERSIONS = "1.21.5"
+        private const val SUPPORTED_PLATFORMS = "Paper"
+        private const val UNSUPPORTED_PLATFORM_MSG =
+            "This plugin requires a supported server platform. Supported platforms: ${SUPPORTED_PLATFORMS}."
+        private const val UNSUPPORTED_VERSION_MSG =
+            "This plugin requires a supported server version. Supported versions: ${SUPPORTED_VERSIONS}."
 
         val PREFIX: String = "${"[".mangoFmt(true)}${"VanillaPlus".fireFmt()}${"]".mangoFmt()}"
 
@@ -30,8 +30,8 @@ class VanillaPlus : JavaPlugin() {
     /** Called when the plugin is enabled. */
     override fun onEnable() {
         when {
-            !isSupportedVersion() -> disablePlugin(UNSUPPORTED_VERSION_MSG)
-            !isSupportedPlatform() -> disablePlugin(UNSUPPORTED_PLATFORM_MSG)
+            !server.version.contains(SUPPORTED_VERSIONS) -> disablePlugin(UNSUPPORTED_VERSION_MSG)
+            !server.name.contains(SUPPORTED_PLATFORMS) -> disablePlugin(UNSUPPORTED_PLATFORM_MSG)
             else -> {
                 ConfigManager.load()
                 Perms
@@ -48,10 +48,4 @@ class VanillaPlus : JavaPlugin() {
         logger.severe(msg)
         server.pluginManager.disablePlugin(this)
     }
-
-    /** Check if the server version is supported. */
-    private fun isSupportedVersion() = SUPPORTED_VERSIONS.any(server.version::contains)
-
-    /** Check if the server platform is supported. */
-    private fun isSupportedPlatform() = SUPPORTED_PLATFORMS.any(server.name::contains)
 }
