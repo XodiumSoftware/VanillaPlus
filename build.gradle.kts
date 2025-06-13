b88c74c10b084e78ae859031d15acdc75776e917
Illyrius <28700752+illyrius666@users.noreply.github.com>
2025-06-13 11:34:30 +0200

    Update build.gradle.kts
    v+
    
    Signed-off-by: Illyrius <28700752+illyrius666@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 group = "org.xodium.vanillaplus"
-version = "1.10.0"
+version = "1.10.1"
 description = "Minecraft plugin that enhances the base gameplay."
 
 var author: String = "Xodium"
