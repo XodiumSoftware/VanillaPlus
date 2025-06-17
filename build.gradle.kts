54c26650e6146abf4c51d47c6f0535028e242086
illyrius666 <fitimq@live.nl>
2025-06-17 21:49:22 +0200

    Remove WorldEdit dependency from build.gradle.kts and clean up paper-plugin.yml
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,6 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:$apiVersion-R0.1-SNAPSHOT")
-    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.3.14") //TODO("Move away from WorldEdit")
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.19.1")
