5d0f06e0e7636ce47e1112ee117caa550af3a8a3
illyrius666 <fitimq@live.nl>
2025-06-23 15:49:36 +0200

    feat: enhance chat functionality with clickable player names and hover text
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/modules/ChatModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/ChatModule.kt
@@ -6,6 +6,8 @@
 package org.xodium.vanillaplus.modules
 
 import io.papermc.paper.event.player.AsyncChatEvent
+import net.kyori.adventure.text.event.ClickEvent
+import net.kyori.adventure.text.event.HoverEvent
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import org.bukkit.event.EventHandler
@@ -13,6 +15,7 @@ import org.bukkit.event.EventPriority
 import org.xodium.vanillaplus.interfaces.ModuleInterface
 import org.xodium.vanillaplus.managers.ConfigManager
 import org.xodium.vanillaplus.utils.ExtUtils.mm
+import org.xodium.vanillaplus.utils.FmtUtils.fireFmt
 
 class ChatModule : ModuleInterface {
     override fun enabled(): Boolean = ConfigManager.data.chatModule.enabled
@@ -20,9 +23,14 @@ class ChatModule : ModuleInterface {
     @EventHandler(priority = EventPriority.HIGHEST)
     fun on(event: AsyncChatEvent) {
         if (!enabled()) return
-        event.renderer { _, displayName, message, _ ->
+        event.renderer { source, displayName, message, _ ->
             ConfigManager.data.chatModule.chatFormat.mm(
-                Placeholder.component("player", displayName),
+                Placeholder.component(
+                    "player",
+                    displayName
+                        .clickEvent(ClickEvent.suggestCommand("/tell ${source.name} "))
+                        .hoverEvent(HoverEvent.showText("Click to Whisper".fireFmt().mm()))
+                ),
                 Placeholder.component("message", PlainTextComponentSerializer.plainText().serialize(message).mm()),
             )
         }
