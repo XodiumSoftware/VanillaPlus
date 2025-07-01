0836e8cd4ebe51126cf01d0053c62e3354a56cd0
illyrius666 <fitimq@live.nl>
2025-07-01 14:12:08 +0200

    feat: add quest management event handling for player joins
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/modules/QuestModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/QuestModule.kt
@@ -11,6 +11,9 @@ import eu.endercentral.crazy_advancements.advancement.AdvancementDisplay
 import eu.endercentral.crazy_advancements.advancement.AdvancementVisibility
 import eu.endercentral.crazy_advancements.manager.AdvancementManager
 import org.bukkit.Material
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.player.PlayerJoinEvent
 import org.xodium.vanillaplus.VanillaPlus.Companion.instance
 import org.xodium.vanillaplus.interfaces.ModuleInterface
 
@@ -19,28 +22,40 @@ class QuestModule : ModuleInterface<QuestModule.Config> {
 
     override fun enabled(): Boolean = config.enabled
 
+    private val advancementManager = AdvancementManager(
+        NameKey(
+            instance::class.simpleName!!.lowercase(),
+            QuestModule::class.simpleName!!.lowercase()
+        ),
+    )
+
     init {
         if (enabled()) {
-            val advancementManager = AdvancementManager(
-                NameKey(
-                    instance::class.simpleName!!.lowercase(),
-                    QuestModule::class.simpleName!!.lowercase()
-                ),
-            )
-
             val rootDisplay = AdvancementDisplay(
                 Material.GRASS_BLOCK,
                 "Quests",
                 "All your quests in one place",
                 AdvancementDisplay.AdvancementFrame.TASK,
                 AdvancementVisibility.ALWAYS
+            ).apply {
+                background(NameKey("minecraft", "block/red_terracotta.png"))
+            }
+            val rootAdvancement = Advancement(
+                null,
+                NameKey(
+                    instance::class.simpleName!!.lowercase(),
+                    "${QuestModule::class.simpleName!!.lowercase()}/root"
+                ),
+                rootDisplay
             )
-
-            rootDisplay.backgroundTexture = "textures/gui/advancements/backgrounds/stone.png"
-
-            val rootAdvancement = Advancement(null, NameKey("vanillaplus", "quests/root"), rootDisplay)
             advancementManager.addAdvancement(rootAdvancement)
         }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    fun on(event: PlayerJoinEvent) {
+        if (!enabled()) return
+        advancementManager.addPlayer(event.player)
     }
 
     data class Config(
