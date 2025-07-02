f31cb8b4924921f8e1af71830dbc04af9d2ba068
illyrius666 <fitimq@live.nl>
2025-07-02 13:20:50 +0200

    feat: update quest progress display with a styled progress indicator
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/modules/QuestModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/QuestModule.kt
@@ -157,7 +157,7 @@ class QuestModule : ModuleInterface<QuestModule.Config> {
                 val progress = quest.progress
                 val required = task?.amount ?: 0
                 val progressBar = createProgressBar(progress, required)
-                lore.add("<b>Progress:</b> $progressBar <b>($progress/$required)</b>") //FIX
+                lore.add("<b>⏳</b> $progressBar ($progress/$required)".roseFmt())
             }
         }
         lore.add("")
