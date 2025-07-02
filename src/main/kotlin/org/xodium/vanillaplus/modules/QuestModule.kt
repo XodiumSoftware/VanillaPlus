c43e2708556dd4a904e637db76a198fcb04974af
illyrius666 <fitimq@live.nl>
2025-07-02 07:18:53 +0200

    feat: add quests command and permissions to QuestModule
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/modules/QuestModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/QuestModule.kt
@@ -5,12 +5,46 @@
 
 package org.xodium.vanillaplus.modules
 
+import io.papermc.paper.command.brigadier.Commands
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemStack
+import org.bukkit.permissions.Permission
+import org.bukkit.permissions.PermissionDefault
+import org.xodium.vanillaplus.VanillaPlus.Companion.instance
+import org.xodium.vanillaplus.data.CommandData
 import org.xodium.vanillaplus.interfaces.ModuleInterface
+import org.xodium.vanillaplus.utils.Utils.tryCatch
 
 class QuestModule : ModuleInterface<QuestModule.Config> {
     override val config: Config = Config()
 
     override fun enabled(): Boolean = config.enabled
+
+    override fun cmds(): CommandData? {
+        return CommandData(
+            listOf(
+                Commands.literal("quests")
+                    .requires { it.sender.hasPermission(perms()[0]) }
+                    .executes { ctx -> ctx.tryCatch { quests() } }
+            ),
+            "List all quests",
+            listOf("q")
+        )
+    }
+
+    override fun perms(): List<Permission> {
+        return listOf(
+            Permission(
+                "${instance::class.simpleName}.quests.use".lowercase(),
+                "Allows use of the quests command",
+                PermissionDefault.TRUE
+            )
+        )
+    }
+
+    private fun quests(): Inventory {}
+
+    private fun questItem(): ItemStack {}
 
     data class Config(
         override var enabled: Boolean = true
