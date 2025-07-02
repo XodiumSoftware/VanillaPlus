b3f77137e4e33da85989e7b50ef8ba69b2464724
illyrius666 <fitimq@live.nl>
2025-07-02 07:22:49 +0200

    feat: enhance quest inventory and item creation methods
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/modules/QuestModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/QuestModule.kt
@@ -6,6 +6,8 @@
 package org.xodium.vanillaplus.modules
 
 import io.papermc.paper.command.brigadier.Commands
+import org.bukkit.Material
+import org.bukkit.event.inventory.InventoryType
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 import org.bukkit.permissions.Permission
@@ -13,6 +15,8 @@ import org.bukkit.permissions.PermissionDefault
 import org.xodium.vanillaplus.VanillaPlus.Companion.instance
 import org.xodium.vanillaplus.data.CommandData
 import org.xodium.vanillaplus.interfaces.ModuleInterface
+import org.xodium.vanillaplus.utils.ExtUtils.mm
+import org.xodium.vanillaplus.utils.FmtUtils.fireFmt
 import org.xodium.vanillaplus.utils.Utils.tryCatch
 
 class QuestModule : ModuleInterface<QuestModule.Config> {
@@ -27,7 +31,7 @@ class QuestModule : ModuleInterface<QuestModule.Config> {
                     .requires { it.sender.hasPermission(perms()[0]) }
                     .executes { ctx -> ctx.tryCatch { quests() } }
             ),
-            "List all quests",
+            "Lists all quests",
             listOf("q")
         )
     }
@@ -42,9 +46,28 @@ class QuestModule : ModuleInterface<QuestModule.Config> {
         )
     }
 
-    private fun quests(): Inventory {}
+    /**
+     * Creates an inventory for quests.
+     * @return an Inventory with 5 slots, each containing a quest item.
+     */
+    private fun quests(): Inventory {
+        return instance.server.createInventory(null, InventoryType.HOPPER, "Quests".fireFmt().mm()).apply {
+            setItem(0, questItem())
+            setItem(1, questItem())
+            setItem(2, questItem())
+            setItem(3, questItem())
+            setItem(4, questItem())
+        }
+    }
 
-    private fun questItem(): ItemStack {}
+    /**
+     * Creates a quest item with a specific material.
+     * @param material the Material for the quest item, defaults to STONE.
+     * @return an ItemStack representing the quest item.
+     */
+    private fun questItem(material: Material): ItemStack {
+        return ItemStack.of(material)
+    }
 
     data class Config(
         override var enabled: Boolean = true
