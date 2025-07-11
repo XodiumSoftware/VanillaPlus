42e6d4ba2f90c582575764c1d0ff46854bbe42c1
illyrius666 <fitimq@live.nl>
2025-07-11 08:24:34 +0200

    add basic nametag functionality for players on join

--- src/main/kotlin/org/xodium/vanillaplus/modules/NameTagModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/NameTagModule.kt
@@ -1,8 +1,12 @@
 package org.xodium.vanillaplus.modules
 
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.scoreboard.Scoreboard
+import org.xodium.vanillaplus.VanillaPlus.Companion.instance
 import org.xodium.vanillaplus.interfaces.ModuleInterface
+import org.xodium.vanillaplus.utils.ExtUtils.mm
 
 
 class NameTagModule : ModuleInterface<NameTagModule.Config> {
@@ -13,10 +17,29 @@ class NameTagModule : ModuleInterface<NameTagModule.Config> {
     @EventHandler
     fun on(event: PlayerJoinEvent) {
         if (!enabled()) return
-        nametag()
+        val player = event.player
+        player.nametag("test", "test")
+        player.nametag()
     }
 
-    private fun nametag(): Unit = TODO()
+    /** Gets the nametag for a player. */
+    private fun Player.nametag(): Scoreboard = this.scoreboard
+
+    /**
+     * Sets the nametag for a player with a prefix and suffix.
+     * @param prefix The prefix to be added to the nametag.
+     * @param suffix The suffix to be added to the nametag.
+     */
+    private fun Player.nametag(prefix: String, suffix: String) {
+        instance.server.scoreboardManager.mainScoreboard.let { scoreboard ->
+            (scoreboard.getTeam(this.name) ?: scoreboard.registerNewTeam(this.name)).apply {
+                prefix(prefix.mm())
+                suffix(suffix.mm())
+                addEntry(this.name)
+            }
+            this.scoreboard = scoreboard
+        }
+    }
 
     data class Config(
         override var enabled: Boolean = true
