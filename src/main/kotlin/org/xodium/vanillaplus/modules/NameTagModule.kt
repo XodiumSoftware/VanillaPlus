43dd531a50f4e12f759f313e10ec4dba194f66c0
illyrius666 <fitimq@live.nl>
2025-07-11 08:10:24 +0200

    init

--- src/main/kotlin/org/xodium/vanillaplus/modules/NameTagModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/NameTagModule.kt
@@ -1,5 +1,7 @@
 package org.xodium.vanillaplus.modules
 
+import org.bukkit.event.EventHandler
+import org.bukkit.event.player.PlayerJoinEvent
 import org.xodium.vanillaplus.interfaces.ModuleInterface
 
 
@@ -7,6 +9,14 @@ class NameTagModule : ModuleInterface<NameTagModule.Config> {
     override val config: Config = Config()
 
     override fun enabled(): Boolean = config.enabled
+
+    @EventHandler
+    fun on(event: PlayerJoinEvent) {
+        if (!enabled()) return
+        nametag()
+    }
+
+    private fun nametag(): Unit = TODO()
 
     data class Config(
         override var enabled: Boolean = true
