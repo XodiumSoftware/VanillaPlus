ae0f7982a1eb4ef3ca1e50ddd51e936887dabad1
illyrius666 <fitimq@live.nl>
2025-07-15 15:20:32 +0200

    woops

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 group = "org.xodium.vanillaplus"
-version = "1.14.1"
+version = "1.14.4"
 description = "Minecraft plugin that enhances the base gameplay."
 
 var author: String = "Xodium"
