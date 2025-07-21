21663be5f1933c361b3dc619170f819f5b48c9ff
illyrius666 <fitimq@live.nl>
2025-07-21 09:36:59 +0200

    cleanup

--- src/main/kotlin/org/xodium/vanillaplus/modules/DimensionsModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/DimensionsModule.kt
@@ -1,19 +1,13 @@
 package org.xodium.vanillaplus.modules
 
-import org.bukkit.Location
-import org.bukkit.Material
 import org.bukkit.World
-import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.entity.EntityPortalEvent
 import org.bukkit.event.player.PlayerPortalEvent
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause
 import org.bukkit.event.world.PortalCreateEvent
-import org.xodium.vanillaplus.VanillaPlus.Companion.instance
 import org.xodium.vanillaplus.interfaces.ModuleInterface
-import org.xodium.vanillaplus.utils.ExtUtils.mm
-import org.xodium.vanillaplus.utils.FmtUtils.fireFmt
 
 /** Represents a module handling dimension mechanics within the system. */
 class DimensionsModule : ModuleInterface<DimensionsModule.Config> {
@@ -47,44 +41,7 @@ class DimensionsModule : ModuleInterface<DimensionsModule.Config> {
 
         if (event.world.environment == World.Environment.NETHER
             && event.reason == PortalCreateEvent.CreateReason.FIRE
-        ) {
-            val portalBlocks = event.blocks
-            if (portalBlocks.isEmpty()) return
-
-            val portalLocation = portalBlocks.first().location
-            val overworld =
-                instance.server.worlds.firstOrNull { it.environment == World.Environment.NORMAL } ?: return
-            if (!hasLinkedOverworldPortal(overworld, portalLocation)) {
-                event.isCancelled = true
-
-                val player = event.entity as? Player
-                player?.sendActionBar(
-                    "You can only light a portal in the Nether if a corresponding portal already exists in the Overworld".fireFmt()
-                        .mm()
-                )
-            }
-        }
-    }
-
-    /**
-     * Checks if there is a linked portal in the Overworld near the given Nether portal location.
-     * @param overworld The Overworld world instance.
-     * @param netherLocation The location of the Nether portal block.
-     * @return True if a linked portal exists, false otherwise.
-     */
-    private fun hasLinkedOverworldPortal(overworld: World, netherLocation: Location): Boolean {
-        val overworldX = netherLocation.blockX * 8
-        val overworldZ = netherLocation.blockZ * 8
-        val searchRadius = 16
-        return sequence {
-            for (x in (overworldX - searchRadius)..(overworldX + searchRadius)) {
-                for (z in (overworldZ - searchRadius)..(overworldZ + searchRadius)) {
-                    for (y in overworld.minHeight until overworld.maxHeight) {
-                        yield(overworld.getBlockAt(x, y, z))
-                    }
-                }
-            }
-        }.any { it.type == Material.NETHER_PORTAL }
+        ) event.isCancelled = true
     }
 
     data class Config(
