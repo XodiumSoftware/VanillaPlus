2f7e8021bc23b9c8ff7a1d13b1876a63c810b916
illyrius666 <fitimq@live.nl>
2025-07-21 08:13:02 +0200

    cancel portal ignite when trying to create new portal in nether

--- src/main/kotlin/org/xodium/vanillaplus/modules/DimensionsModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/DimensionsModule.kt
@@ -6,6 +6,7 @@ import org.bukkit.event.EventPriority
 import org.bukkit.event.entity.EntityPortalEvent
 import org.bukkit.event.player.PlayerPortalEvent
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause
+import org.bukkit.event.world.PortalCreateEvent
 import org.xodium.vanillaplus.interfaces.ModuleInterface
 
 /** Represents a module handling dimension mechanics within the system. */
@@ -13,6 +14,15 @@ class DimensionsModule : ModuleInterface<DimensionsModule.Config> {
     override val config: Config = Config()
 
     override fun enabled(): Boolean = config.enabled
+
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    fun on(event: PortalCreateEvent) {
+        if (!enabled()) return
+
+        if (event.world.environment == World.Environment.NETHER
+            && event.reason == PortalCreateEvent.CreateReason.FIRE
+        ) event.isCancelled = true
+    }
 
     @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     fun on(event: PlayerPortalEvent) {
