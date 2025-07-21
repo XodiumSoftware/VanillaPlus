7895bf0d0fe8ec6665f20db5e02709e9d539f558
illyrius666 <fitimq@live.nl>
2025-07-21 08:23:27 +0200

    make it so portals can we lit again in the nether if there exist a corresponding portal in the overworld

--- src/main/kotlin/org/xodium/vanillaplus/modules/DimensionsModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/DimensionsModule.kt
@@ -1,28 +1,25 @@
 package org.xodium.vanillaplus.modules
 
+import org.bukkit.Location
+import org.bukkit.Material
 import org.bukkit.World
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.entity.EntityPortalEvent
 import org.bukkit.event.player.PlayerPortalEvent
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause
 import org.bukkit.event.world.PortalCreateEvent
+import org.xodium.vanillaplus.VanillaPlus.Companion.instance
 import org.xodium.vanillaplus.interfaces.ModuleInterface
+import org.xodium.vanillaplus.utils.ExtUtils.mm
+import org.xodium.vanillaplus.utils.FmtUtils.fireFmt
 
 /** Represents a module handling dimension mechanics within the system. */
 class DimensionsModule : ModuleInterface<DimensionsModule.Config> {
     override val config: Config = Config()
 
     override fun enabled(): Boolean = config.enabled
-
-    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
-    fun on(event: PortalCreateEvent) {
-        if (!enabled()) return
-
-        if (event.world.environment == World.Environment.NETHER
-            && event.reason == PortalCreateEvent.CreateReason.FIRE
-        ) event.isCancelled = true
-    }
 
     @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     fun on(event: PlayerPortalEvent) {
@@ -42,6 +39,63 @@ class DimensionsModule : ModuleInterface<DimensionsModule.Config> {
         if (event.entity.world.environment == World.Environment.NETHER) {
             event.canCreatePortal = false
         }
+    }
+
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    fun on(event: PortalCreateEvent) {
+        if (!enabled()) return
+
+        if (event.world.environment == World.Environment.NETHER
+            && event.reason == PortalCreateEvent.CreateReason.FIRE
+        ) {
+            val portalBlocks = event.blocks
+            if (portalBlocks.isEmpty()) return
+
+            val portalLocation = portalBlocks.first().location
+            val overworld =
+                instance.server.worlds.firstOrNull { it.environment == World.Environment.NORMAL } ?: return
+            if (!hasLinkedOverworldPortal(overworld, portalLocation)) {
+                event.isCancelled = true
+
+                findFirstPlayerNear(portalLocation, 8.0)?.sendActionBar(
+                    "You can only light a portal in the Nether if a corresponding portal already exists in the Overworld".fireFmt()
+                        .mm()
+                )
+            }
+        }
+    }
+
+    /**
+     * Checks if there is a linked portal in the Overworld near the given Nether portal location.
+     * @param overworld The Overworld world instance.
+     * @param netherLocation The location of the Nether portal block.
+     * @return True if a linked portal exists, false otherwise.
+     */
+    private fun hasLinkedOverworldPortal(overworld: World, netherLocation: Location): Boolean {
+        val overworldLoc = Location(overworld, netherLocation.x * 8, netherLocation.y, netherLocation.z * 8)
+        val searchRadius = 16
+        for (x in (overworldLoc.blockX - searchRadius)..(overworldLoc.blockX + searchRadius)) {
+            for (z in (overworldLoc.blockZ - searchRadius)..(overworldLoc.blockZ + searchRadius)) {
+                for (y in overworld.minHeight until overworld.maxHeight) {
+                    if (overworld.getBlockAt(x, y, z).type == Material.NETHER_PORTAL) {
+                        return true
+                    }
+                }
+            }
+        }
+        return false
+    }
+
+    /**
+     * Finds the first player near a given location within the specified radius.
+     * @param location The centre location to search around.
+     * @param radius The search radius.
+     * @return The first nearby player, or null if none found.
+     */
+    private fun findFirstPlayerNear(location: Location, radius: Double): Player? {
+        return location.world.getNearbyEntities(location, radius, radius, radius)
+            .filterIsInstance<Player>()
+            .firstOrNull()
     }
 
     data class Config(
