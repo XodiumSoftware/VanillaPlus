175c2d9e740113aefcc0e73063ab0b2565ea7081
illyrius666 <fitimq@live.nl>
2025-08-05 18:34:44 +0200

    refactor MobsModule.kt: centralize grief prevention logic in `shouldCancelGrief` function
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/modules/MobsModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/MobsModule.kt
@@ -4,10 +4,8 @@ package org.xodium.vanillaplus.modules
 
 import org.bukkit.entity.*
 import org.bukkit.event.EventHandler
-import org.bukkit.event.block.BlockIgniteEvent
 import org.bukkit.event.entity.EntityChangeBlockEvent
 import org.bukkit.event.entity.EntityExplodeEvent
-import org.bukkit.event.entity.ProjectileHitEvent
 import org.xodium.vanillaplus.interfaces.ModuleInterface
 
 /** Represents a module handling mobs mechanics within the system. */
@@ -15,53 +13,41 @@ internal class MobsModule : ModuleInterface<MobsModule.Config> {
     override val config: Config = Config()
 
     @EventHandler
-    fun on(event: EntityExplodeEvent) {
-        if (!enabled()) return
-        if (when (event.entity) {
-                is Creeper -> config.disableCreeperGrief
-                is Wither -> config.disableWitherGrief
-                is WitherSkull -> config.disableWitherSkullGrief
-                is EnderDragon -> config.disableEnderDragonGrief
-                else -> false
-            }
-        ) {
-            event.blockList().clear()
-        }
-    }
-
-    @EventHandler
-    fun on(event: ProjectileHitEvent) {
-        if (!enabled() || !config.disableGhastGrief) return
-
-        val projectile = event.entity
-        if (projectile is Fireball && projectile.shooter is Ghast) {
-            event.hitBlock?.let {
-                event.isCancelled = true
-                projectile.remove()
-            }
-        }
-    }
-
-    @EventHandler
     fun on(event: EntityChangeBlockEvent) {
         if (!enabled()) return
-        if (event.entity is Enderman && config.disableEndermanGrief) event.isCancelled = true
+        if (shouldCancelGrief(event.entity)) event.isCancelled = true
     }
 
     @EventHandler
-    fun on(event: BlockIgniteEvent) {
+    fun on(event: EntityExplodeEvent) {
         if (!enabled()) return
-        if (event.ignitingEntity is Blaze && config.disableBlazeGrief) event.isCancelled = true
+        if (shouldCancelGrief(event.entity)) event.blockList().clear()
     }
+
+    /**
+     * Determines whether an entity's griefing behaviour should be cancelled based on configuration settings.
+     * @param entity The entity whose griefing behaviour is being evaluated.
+     * @return `true` if the entity's griefing behaviour should be cancelled; `false` otherwise.
+     */
+    private fun shouldCancelGrief(entity: Entity): Boolean =
+        when (entity) {
+            is WitherSkull -> config.disableWitherGrief
+            is Fireball -> config.disableGhastGrief
+            is Blaze -> config.disableBlazeGrief
+            is Creeper -> config.disableCreeperGrief
+            is EnderDragon -> config.disableEnderDragonGrief
+            is Enderman -> config.disableEndermanGrief
+            is Wither -> config.disableWitherGrief
+            else -> false
+        }
 
     data class Config(
         override var enabled: Boolean = true,
+        var disableBlazeGrief: Boolean = true,
         var disableCreeperGrief: Boolean = true,
-        var disableGhastGrief: Boolean = true,
-        var disableWitherGrief: Boolean = true,
-        var disableWitherSkullGrief: Boolean = true,
         var disableEnderDragonGrief: Boolean = true,
         var disableEndermanGrief: Boolean = true,
-        var disableBlazeGrief: Boolean = true,
+        var disableGhastGrief: Boolean = true,
+        var disableWitherGrief: Boolean = true,
     ) : ModuleInterface.Config
 }
