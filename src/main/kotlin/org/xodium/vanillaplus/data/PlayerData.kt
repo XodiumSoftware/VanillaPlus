7ee04974869ca56b7d0c0d1346df447572d8ccbd
illyrius666 <fitimq@live.nl>
2025-08-12 10:54:10 +0200

    broadcast rc1
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/data/PlayerData.kt
+++ src/main/kotlin/org/xodium/vanillaplus/data/PlayerData.kt
@@ -18,11 +18,9 @@ import kotlin.io.path.writeText
 /**
  * Represents the data structure for player data.
  * @param nickname The [nickname] of the player, if set.
- * @param tips Whether the player has enabled/disabled tips.
- * */
+ */
 internal data class PlayerData(
     val nickname: String? = null,
-    val tips: Boolean = false,
 ) {
     companion object {
         private val mapper =
--- src/main/kotlin/org/xodium/vanillaplus/modules/BroadcastModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/BroadcastModule.kt
@@ -1,13 +1,8 @@
 package org.xodium.vanillaplus.modules
 
-import io.papermc.paper.command.brigadier.Commands
-import org.bukkit.entity.Player
-import org.bukkit.permissions.Permission
-import org.bukkit.permissions.PermissionDefault
 import org.xodium.vanillaplus.VanillaPlus.Companion.instance
-import org.xodium.vanillaplus.data.CommandData
 import org.xodium.vanillaplus.interfaces.ModuleInterface
-import org.xodium.vanillaplus.utils.ExtUtils.tryCatch
+import org.xodium.vanillaplus.utils.ExtUtils.mm
 import org.xodium.vanillaplus.utils.FmtUtils.fireFmt
 import org.xodium.vanillaplus.utils.FmtUtils.roseFmt
 import org.xodium.vanillaplus.utils.FmtUtils.spellbiteFmt
@@ -16,35 +11,33 @@ import org.xodium.vanillaplus.utils.FmtUtils.spellbiteFmt
 internal class BroadcastModule : ModuleInterface<BroadcastModule.Config> {
     override val config: Config = Config()
 
-    override fun cmds(): List<CommandData> =
-        listOf(
-            CommandData(
-                Commands
-                    .literal("tips")
-                    .requires { it.sender.hasPermission(perms()[0]) }
-                    .executes { ctx -> ctx.tryCatch { broadcast(it.sender as Player) } },
-                "Allows players to set or remove their nickname",
-                listOf("nick"),
-            ),
-        )
+    init {
+        if (enabled()) broadcast()
+    }
 
-    override fun perms(): List<Permission> =
-        listOf(
-            Permission(
-                "${instance::class.simpleName}.tips".lowercase(),
-                "Allows use of the tips command",
-                PermissionDefault.TRUE,
-            ),
+    private fun broadcast() {
+        instance.server.scheduler.runTaskTimer(
+            instance,
+            Runnable {
+                config.broadcastData.forEach { instance.server.broadcast(it.mm()) }
+            },
+            config.broadcastDelay,
+            config.broadcastPeriod,
         )
-
-    private fun broadcast(player: Player) {
     }
 
     data class Config(
         override var enabled: Boolean = true,
-        var tips: List<String> =
+        var broadcastData: List<String> =
             listOf(
-                "${"Signs can make use of ".spellbiteFmt()}<click:open_url:'https://docs.advntr.dev/minimessage/format.html'><hover:show_text:'${"Click Me!".fireFmt()}'>${"[<u>MiniMessage</u>]".roseFmt()}${" formatting!".spellbiteFmt()}",
+                "${l18n.broadcastPrefix} ${"Signs can make use of ".spellbiteFmt()}<click:open_url:'https://docs.advntr.dev/minimessage/format.html'><hover:show_text:'${"Click Me!".fireFmt()}'>${"[<u>MiniMessage</u>]".roseFmt()}${" formatting!".spellbiteFmt()}",
             ),
-    ) : ModuleInterface.Config
+        var broadcastDelay: Long = 100L,
+        var broadcastPeriod: Long = 1000L,
+        var l18n: L18n = L18n(),
+    ) : ModuleInterface.Config {
+        data class L18n(
+            var broadcastPrefix: String = "Broadcast <gray>🟅".fireFmt(),
+        )
+    }
 }
