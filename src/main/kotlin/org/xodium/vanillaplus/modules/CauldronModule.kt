3febc442cf00bbb6d08d551640d01fe1447532ff
illyrius666 <fitimq@live.nl>
2025-09-10 18:03:55 +0200

    take 2
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/modules/CauldronModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/CauldronModule.kt
@@ -1,72 +1,49 @@
 package org.xodium.vanillaplus.modules
 
 import org.bukkit.Material
-import org.xodium.vanillaplus.VanillaPlus.Companion.instance
+import org.bukkit.Tag
+import org.bukkit.block.data.Levelled
+import org.bukkit.event.EventHandler
+import org.bukkit.event.player.PlayerDropItemEvent
+import org.bukkit.inventory.ItemStack
 import org.xodium.vanillaplus.interfaces.ModuleInterface
 
 /** Represents a module handling cauldron mechanics within the system. */
 internal class CauldronModule : ModuleInterface<CauldronModule.Config> {
     override val config: Config = Config()
 
-    init {
-        if (enabled()) {
-            instance.server.scheduler.runTaskTimer(
-                instance,
-                Runnable {
-                    for (world in instance.server.worlds) {
-                        for (chunk in world.loadedChunks) {
-                            for (x in 0..15) {
-                                for (y in 0 until world.maxHeight) {
-                                    for (z in 0..15) {
-                                        val block = chunk.getBlock(x, y, z)
-                                        if (block.type != Material.WATER_CAULDRON) continue
+    @EventHandler
+    fun on(event: PlayerDropItemEvent) {
+        if (!enabled()) return
 
-                                        val nearbyItems = block.world
-                                            .getNearbyEntities(block.location.add(0.5, 0.5, 0.5), 0.5, 1.0, 0.5)
-                                            .filter { it is org.bukkit.entity.Item }
-                                            .map { it as org.bukkit.entity.Item }
+        val item = event.itemDrop
+        val blockBelow = item.location.block
+        if (blockBelow.type != Material.WATER_CAULDRON) return
 
-                                        for (item in nearbyItems) {
-                                            val mat = when {
-                                                config.convertDirt && item.itemStack.type == Material.DIRT -> Material.MUD
-                                                config.convertConcretePowder && org.bukkit.Tag.CONCRETE_POWDER.isTagged(
-                                                    item.itemStack.type
-                                                ) ->
-                                                    Material.entries.firstOrNull {
-                                                        it.name == item.itemStack.type.name.removeSuffix("_POWDER")
-                                                    }
+        val mat = when {
+            config.convertDirt && item.itemStack.type == Material.DIRT -> Material.MUD
+            config.convertConcretePowder && Tag.CONCRETE_POWDER.isTagged(item.itemStack.type) ->
+                Material.entries.firstOrNull { it.name == item.itemStack.type.name.removeSuffix("_POWDER") }
 
-                                                else -> null
-                                            }
+            else -> null
+        }
 
-                                            mat?.let {
-                                                println("Item ${item.itemStack.type} dropped in cauldron, converting to $it at ${block.location}")
-                                                item.remove()
-                                                block.type = it
-
-                                                val data = block.blockData
-                                                if (data is org.bukkit.block.data.Levelled) {
-                                                    data.level = (data.level - 1).coerceAtLeast(0)
-                                                    block.blockData = data
-                                                    println("Cauldron water level decreased to ${data.level}")
-                                                    if (data.level == 0) {
-                                                        block.type = Material.CAULDRON
-                                                        println("Cauldron is now empty")
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                },
-                1L,
-                2L,
+        mat?.let {
+            item.remove()
+            blockBelow.world.dropItemNaturally(
+                blockBelow.location.add(0.5, 0.5, 0.5),
+                ItemStack.of(it)
             )
+
+            val data = blockBelow.blockData
+            if (data is Levelled) {
+                data.level = (data.level - 1).coerceAtLeast(0)
+                blockBelow.blockData = data
+                if (data.level == 0) blockBelow.type = Material.CAULDRON
+            }
         }
     }
+
 
     data class Config(
         override var enabled: Boolean = true,
