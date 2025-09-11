a6f86e98f1b3ef5d68d3d3938be02339f41f7600
illyrius666 <fitimq@live.nl>
2025-09-11 13:58:15 +0200

    added sound and changed the message a little bit
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/modules/WanderingTraderModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/WanderingTraderModule.kt
@@ -1,11 +1,14 @@
 package org.xodium.vanillaplus.modules
 
+import net.kyori.adventure.sound.Sound
 import org.bukkit.entity.EntityType
 import org.bukkit.event.EventHandler
 import org.bukkit.event.entity.CreatureSpawnEvent
+import org.xodium.vanillaplus.data.SoundData
 import org.xodium.vanillaplus.interfaces.ModuleInterface
 import org.xodium.vanillaplus.utils.ExtUtils.mm
 import org.xodium.vanillaplus.utils.FmtUtils.mangoFmt
+import org.bukkit.Sound as BukkitSound
 
 /** Represents a module handling wandering trader mechanics within the system. */
 internal class WanderingTraderModule : ModuleInterface<WanderingTraderModule.Config> {
@@ -13,7 +16,7 @@ internal class WanderingTraderModule : ModuleInterface<WanderingTraderModule.Con
 
     @EventHandler
     fun on(event: CreatureSpawnEvent) {
-        if (!config.enabled || event.entityType != EntityType.WANDERING_TRADER) return
+        if (!enabled() || event.entityType != EntityType.WANDERING_TRADER) return
 
         val world = event.location.world ?: return
         val location = event.location
@@ -21,6 +24,7 @@ internal class WanderingTraderModule : ModuleInterface<WanderingTraderModule.Con
         for (player in world.players) {
             if (player.location.distanceSquared(location) <= config.notifyRadius * config.notifyRadius) {
                 player.sendActionBar(config.traderMessage.mm())
+                player.playSound(config.traderSpawnSound.toSound(), Sound.Emitter.self())
             }
         }
     }
@@ -28,6 +32,10 @@ internal class WanderingTraderModule : ModuleInterface<WanderingTraderModule.Con
     data class Config(
         override var enabled: Boolean = true,
         var notifyRadius: Double = 32.0,
-        var traderMessage: String = "A Wandering Trader has arrived nearby!".mangoFmt()
+        var traderMessage: String = "⭐ A Wandering Trader has arrived nearby ⭐".mangoFmt(),
+        var traderSpawnSound: SoundData = SoundData(
+            BukkitSound.ENTITY_EXPERIENCE_ORB_PICKUP,
+            Sound.Source.UI,
+        ),
     ) : ModuleInterface.Config
 }
