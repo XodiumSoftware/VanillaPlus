313a076db2f85aded91031a1a26701b9608888df
illyrius666 <fitimq@live.nl>
2025-09-17 12:05:47 +0200

    refactor
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/modules/PlayerModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/PlayerModule.kt
@@ -6,9 +6,6 @@ import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.SkullMeta
 import org.xodium.vanillaplus.interfaces.ModuleInterface
-import java.io.IOException
-import java.net.HttpURLConnection
-import java.net.URI
 
 /** Represents a module handling player mechanics within the system. */
 internal class PlayerModule : ModuleInterface<PlayerModule.Config> {
@@ -16,8 +13,7 @@ internal class PlayerModule : ModuleInterface<PlayerModule.Config> {
 
     @EventHandler
     fun on(event: PlayerDeathEvent) {
-        if (!enabled()) return
-        if (event.entity.killer == null) return
+        if (!enabled() || event.entity.killer == null) return
 
         val victim = event.entity
 
@@ -25,21 +21,6 @@ internal class PlayerModule : ModuleInterface<PlayerModule.Config> {
         val meta = skull.itemMeta as SkullMeta
         meta.owningPlayer = victim
         skull.itemMeta = meta
-
-        val uuid = victim.uniqueId.toString()
-        val apiUrl = "https://api.minecraft-heads.com/MHAPI/v2/heads?uuid=$uuid"
-        try {
-            val connection = URI(apiUrl).toURL().openConnection() as HttpURLConnection
-            connection.requestMethod = "GET"
-            connection.setRequestProperty("User-Agent", "VanillaPlus")
-            connection.connectTimeout = 3000
-            connection.readTimeout = 3000
-            if (connection.responseCode == HttpURLConnection.HTTP_OK) {
-                connection.inputStream.bufferedReader().readText()
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
 
         victim.world.dropItemNaturally(victim.location, skull)
     }
