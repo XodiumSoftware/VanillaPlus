f3e52415b231da21ddda137ac46a49563ed6a8a3
illyrius666 <fitimq@live.nl>
2025-09-17 12:17:13 +0200

    added customname to skull
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/modules/PlayerModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/PlayerModule.kt
@@ -6,6 +6,8 @@ import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.SkullMeta
 import org.xodium.vanillaplus.interfaces.ModuleInterface
+import org.xodium.vanillaplus.utils.ExtUtils.mm
+import org.xodium.vanillaplus.utils.FmtUtils.fireFmt
 
 /** Represents a module handling player mechanics within the system. */
 internal class PlayerModule : ModuleInterface<PlayerModule.Config> {
@@ -19,6 +21,7 @@ internal class PlayerModule : ModuleInterface<PlayerModule.Config> {
         val itemStack = ItemStack.of(Material.PLAYER_HEAD)
         val itemMeta = itemStack.itemMeta as SkullMeta
         if (itemMeta.setOwningPlayer(entity)) {
+            itemMeta.customName(entity.displayName().append("'s Skull".fireFmt().mm()))
             itemStack.itemMeta = itemMeta
             entity.world.dropItemNaturally(entity.location, itemStack)
         }
