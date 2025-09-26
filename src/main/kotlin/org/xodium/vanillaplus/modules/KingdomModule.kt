d8665760052d781316989e523a13e35a62afaebc
illyrius666 <fitimq@live.nl>
2025-09-26 11:54:01 +0200

    woops
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/modules/KingdomModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/KingdomModule.kt
@@ -1,17 +1,10 @@
-@file:Suppress("ktlint:standard:no-wildcard-imports")
-
 package org.xodium.vanillaplus.modules
 
-import org.xodium.vanillaplus.data.KingdomData
 import org.xodium.vanillaplus.interfaces.ModuleInterface
-import java.util.*
 
 /** Represents a module handling kingdom mechanics within the system. */
 internal class KingdomModule : ModuleInterface<KingdomModule.Config> {
     override val config: Config = Config()
-
-    private val kingdoms: MutableMap<UUID, KingdomData> = mutableMapOf()
-    private val playerKingdoms: MutableMap<UUID, UUID> = mutableMapOf()
 
     data class Config(
         override var enabled: Boolean = true,
