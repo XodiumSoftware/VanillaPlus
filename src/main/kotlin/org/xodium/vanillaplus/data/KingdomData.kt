7123e746479bf68ae1560cfda660fc60c76c886e
illyrius666 <fitimq@live.nl>
2025-09-26 11:53:30 +0200

    +
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/data/KingdomData.kt
+++ src/main/kotlin/org/xodium/vanillaplus/data/KingdomData.kt
@@ -8,15 +8,48 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import org.bukkit.entity.Player
 import org.xodium.vanillaplus.VanillaPlus.Companion.instance
 import java.io.IOException
 import java.util.*
 import kotlin.io.path.createDirectories
 import kotlin.io.path.writeText
 
+/**
+ * Represents a kingdom in the VanillaPlus plugin.
+ * @property id The unique identifier of the kingdom.
+ * @property name The display name of the kingdom.
+ * @property ruler The UUID of the player who rules this kingdom.
+ * @property creationDate The date when the kingdom was created.
+ * @property members Mutable set of UUIDs representing kingdom members (excluding the ruler).
+ */
 data class KingdomData(
-    val kingdom: String,
+    val id: UUID,
+    val name: String,
+    val ruler: UUID,
+    val creationDate: Date,
+    val members: MutableSet<UUID> = mutableSetOf(),
 ) {
+    /**
+     * Gets the Player object of the kingdom's ruler if they are online.
+     * @return The ruler as a Player object, or null if the ruler is offline.
+     */
+    fun getRuler(): Player? = instance.server.getPlayer(ruler)
+
+    /**
+     * Adds a player as a member of the kingdom.
+     * @param player The player to add to the kingdom.
+     * @return true if the player was added successfully, false if they were already a member.
+     */
+    fun addMember(player: Player): Boolean = members.add(player.uniqueId)
+
+    /**
+     * Checks if a player is a member of the kingdom.
+     * @param player The player to check.
+     * @return true if the player is the ruler or a member, false otherwise.
+     */
+    fun isMember(player: Player): Boolean = ruler == player.uniqueId || members.contains(player.uniqueId)
+
     companion object {
         private val mapper =
             jacksonObjectMapper()
