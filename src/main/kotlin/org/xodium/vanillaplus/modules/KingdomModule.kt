f47da0ee11fe3041e2b9bf666705fce5bcc5cb83
illyrius666 <fitimq@live.nl>
2025-09-26 16:27:28 +0200

    added sceptre
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/modules/KingdomModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/KingdomModule.kt
@@ -3,15 +3,16 @@ package org.xodium.vanillaplus.modules
 import dev.triumphteam.gui.paper.Gui
 import dev.triumphteam.gui.paper.container.type.PaperContainerType.hopper
 import dev.triumphteam.gui.paper.kotlin.builder.buildGui
-import io.papermc.paper.command.brigadier.Commands
-import org.bukkit.entity.Player
-import org.bukkit.permissions.Permission
-import org.bukkit.permissions.PermissionDefault
-import org.xodium.vanillaplus.VanillaPlus.Companion.instance
-import org.xodium.vanillaplus.data.CommandData
+import io.papermc.paper.datacomponent.DataComponentTypes
+import io.papermc.paper.datacomponent.item.ItemLore
+import org.bukkit.Material
+import org.bukkit.event.EventHandler
+import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.ItemStack
 import org.xodium.vanillaplus.interfaces.ModuleInterface
 import org.xodium.vanillaplus.utils.ExtUtils.mm
-import org.xodium.vanillaplus.utils.ExtUtils.tryCatch
+import org.xodium.vanillaplus.utils.FmtUtils.mangoFmt
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
@@ -19,31 +20,13 @@ import kotlin.time.Duration.Companion.seconds
 internal class KingdomModule : ModuleInterface<KingdomModule.Config> {
     override val config: Config = Config()
 
-    override fun cmds(): List<CommandData> =
-        listOf(
-            CommandData(
-                Commands
-                    .literal("kingdom")
-                    .requires { it.sender.hasPermission(perms()[0]) }
-                    .executes { ctx ->
-                        ctx.tryCatch {
-                            if (it.sender !is Player) instance.logger.warning("Command can only be executed by a Player!")
-                            gui().open(it.sender as Player)
-                        }
-                    },
-                "This command allows you to open the kingdom gui",
-                listOf("k"),
-            ),
-        )
+    @EventHandler
+    fun on(event: PlayerInteractEvent) {
+        if (!enabled() && event.action != Action.RIGHT_CLICK_AIR && event.action != Action.RIGHT_CLICK_BLOCK) return
 
-    override fun perms(): List<Permission> =
-        listOf(
-            Permission(
-                "${instance::class.simpleName}.kingdom".lowercase(),
-                "Allows use of the kingdom command",
-                PermissionDefault.TRUE,
-            ),
-        )
+        val item = event.item ?: return
+        if (item.type != config.sceptreMaterial) return
+    }
 
     private fun gui(): Gui =
         buildGui {
@@ -52,8 +35,20 @@ internal class KingdomModule : ModuleInterface<KingdomModule.Config> {
             title("Kingdom".mm()) // TODO: change to player kingdom name.
         }
 
+    private fun sceptre(): ItemStack =
+        @Suppress("unstableApiUsage")
+        ItemStack.of(config.sceptreMaterial).apply {
+            setData(DataComponentTypes.ITEM_NAME, config.sceptreItemName.mm())
+            setData(DataComponentTypes.LORE, ItemLore.lore(config.sceptreLore.mm()))
+            setData(DataComponentTypes.ENCHANTMENT_GLINT_OVERRIDE, config.sceptreGlint)
+        }
+
     data class Config(
         override var enabled: Boolean = true,
         var guiSpamPreventionDuration: Duration = 1.seconds,
+        var sceptreMaterial: Material = Material.BLAZE_ROD,
+        var sceptreItemName: String = "Royal Sceptre".mangoFmt(),
+        var sceptreLore: MutableList<String> = mutableListOf("<gray>Right-click to manage your Kingdom</gray>"),
+        var sceptreGlint: Boolean = true,
     ) : ModuleInterface.Config
 }
