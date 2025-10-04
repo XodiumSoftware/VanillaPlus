c9b3eb0a29e5eb6057c512c196b935ab6d60df14
illyrius666 <fitimq@live.nl>
2025-10-04 14:12:50 +0200

    clearing scoreboard
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/modules/ScoreBoardModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/ScoreBoardModule.kt
@@ -1,16 +1,46 @@
 package org.xodium.vanillaplus.modules
 
+import io.papermc.paper.command.brigadier.Commands
+import org.bukkit.entity.Player
+import org.bukkit.permissions.Permission
+import org.bukkit.permissions.PermissionDefault
+import org.bukkit.scoreboard.DisplaySlot
+import org.xodium.vanillaplus.VanillaPlus.Companion.instance
+import org.xodium.vanillaplus.data.CommandData
 import org.xodium.vanillaplus.interfaces.ModuleInterface
+import org.xodium.vanillaplus.utils.ExtUtils.tryCatch
 
 /** Represents a module handling scoreboard mechanics within the system. */
 internal class ScoreBoardModule : ModuleInterface<ScoreBoardModule.Config> {
     override val config: Config = Config()
 
-//    @EventHandler
-//    fun on(event: PlayerAdvancementDoneEvent) {
-//        if (!enabled()) return
-//        instance.server.onlinePlayers.forEach { it.scoreboard = instance.server.scoreboardManager.mainScoreboard }
-//    }
+    override fun cmds(): List<CommandData> =
+        listOf(
+            CommandData(
+                Commands
+                    .literal("leaderboard")
+                    .requires { it.sender.hasPermission(perms()[0]) }
+                    .executes { ctx ->
+                        ctx.tryCatch {
+                            if (it.sender !is Player) instance.logger.warning("Command can only be executed by a Player!")
+                            toggle(it.sender as Player)
+                        }
+                    },
+                "This command allows you to open the leaderboard",
+                listOf("w"),
+            ),
+        )
+
+    override fun perms(): List<Permission> =
+        listOf(
+            Permission(
+                "${instance::class.simpleName}.leaderboard".lowercase(),
+                "Allows use of the leaderboard command",
+                PermissionDefault.TRUE,
+            ),
+        )
+
+    private fun toggle(player: Player) = player.scoreboard.clearSlot(DisplaySlot.SIDEBAR)
 
     data class Config(
         override var enabled: Boolean = true,
