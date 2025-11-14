d7f6e1ac379bef09498cf297b2f5d5e24976f8ae
Illyrius <fitimq@live.nl>
2025-11-14 11:06:52 +0100

    feat: implement night vision effect handling for helmet enchantment
    Signed-off-by: Illyrius <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/enchantments/NightVisionEnchantment.kt
+++ src/main/kotlin/org/xodium/vanillaplus/enchantments/NightVisionEnchantment.kt
@@ -1,6 +1,10 @@
 package org.xodium.vanillaplus.enchantments
 
+import io.papermc.paper.event.entity.EntityEquipmentChangedEvent
 import io.papermc.paper.registry.data.EnchantmentRegistryEntry
+import org.bukkit.entity.Player
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 import org.xodium.vanillaplus.interfaces.EnchantmentInterface
 import org.xodium.vanillaplus.utils.ExtUtils.mm
 
@@ -16,4 +20,22 @@ internal object NightVisionEnchantment : EnchantmentInterface {
             .minimumCost(EnchantmentRegistryEntry.EnchantmentCost.of(TODO()))
             .maximumCost(EnchantmentRegistryEntry.EnchantmentCost.of(TODO()))
             .activeSlots(TODO())
+
+    /**
+     * Handles the equipment change event to apply or remove night vision effect based on the helmet enchantment.
+     * @param event The EntityEquipmentChangedEvent triggered when an entity's equipment changes.
+     */
+    fun nightVision(event: EntityEquipmentChangedEvent) {
+        val player = event.entity as Player
+        val helmet = player.inventory.helmet ?: return
+
+        if (!helmet.hasItemMeta()) return
+        if (helmet.itemMeta.hasEnchant(get())) {
+            player.addPotionEffect(PotionEffect(PotionEffectType.NIGHT_VISION, Int.MAX_VALUE, 0, true, false, true))
+        } else {
+            if (player.hasPotionEffect(PotionEffectType.NIGHT_VISION)) {
+                player.removePotionEffect(PotionEffectType.NIGHT_VISION)
+            }
+        }
+    }
 }
