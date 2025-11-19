dc9c333b0c95d816e8e030823b457ab173ae366b
Illyrius <28700752+illyrius666@users.noreply.github.com>
2025-11-19 20:34:45 +0100

    init (#320)
    Signed-off-by: Illyrius <FitimQ@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/modules/RecipeModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/RecipeModule.kt
@@ -2,6 +2,7 @@ package org.xodium.vanillaplus.modules
 
 import org.xodium.vanillaplus.interfaces.ModuleInterface
 import org.xodium.vanillaplus.recipies.RottenFleshRecipe
+import org.xodium.vanillaplus.recipies.WoodLogRecipe
 
 /** Represents a module handling recipe mechanics within the system. */
 internal class RecipeModule : ModuleInterface<RecipeModule.Config> {
@@ -10,11 +11,13 @@ internal class RecipeModule : ModuleInterface<RecipeModule.Config> {
     init {
         if (enabled()) {
             if (config.rottenFleshToLeatherEnabled) RottenFleshRecipe.register()
+            if (config.woodToLogEnabled) WoodLogRecipe.register()
         }
     }
 
     data class Config(
         override var enabled: Boolean = true,
         var rottenFleshToLeatherEnabled: Boolean = true,
+        var woodToLogEnabled: Boolean = true,
     ) : ModuleInterface.Config
 }
