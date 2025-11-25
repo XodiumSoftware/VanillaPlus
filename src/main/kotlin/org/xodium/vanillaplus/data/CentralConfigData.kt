b35fb7ff99e5bd26e644b4576340a87d466f24e3
Illyrius <fitimq@live.nl>
2025-11-25 11:58:02 +0100

    Refactor CentralConfigData to use non-default constructor parameters for feature configurations
    Signed-off-by: Illyrius <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/data/CentralConfigData.kt
+++ src/main/kotlin/org/xodium/vanillaplus/data/CentralConfigData.kt
@@ -6,6 +6,6 @@ import org.xodium.vanillaplus.features.CauldronFeature
 
 @Serializable
 internal data class CentralConfigData(
-    val booksFeature: BooksFeature.Config = BooksFeature.config,
-    val cauldronFeature: CauldronFeature.Config = CauldronFeature.config,
+    val booksFeature: BooksFeature.Config,
+    val cauldronFeature: CauldronFeature.Config,
 )
