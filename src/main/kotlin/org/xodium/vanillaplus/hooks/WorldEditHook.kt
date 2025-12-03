41bf64c221c70972579d6ea538d6efb67476d263
Illyrius <fitimq@live.nl>
2025-12-03 15:42:17 +0100

    refactor: remove unused CONTAINER_TYPES from MaterialRegistry and change WorldEditHook to internal
    Signed-off-by: Illyrius <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/hooks/WorldEditHook.kt
+++ src/main/kotlin/org/xodium/vanillaplus/hooks/WorldEditHook.kt
@@ -4,7 +4,7 @@ import org.xodium.vanillaplus.VanillaPlus.Companion.instance
 import org.xodium.vanillaplus.modules.TreesModule
 
 /** A utility object for checking plugin availability and handling related dependencies. */
-object WorldEditHook {
+internal object WorldEditHook {
     /**
      * Checks if a specified plugin is available and optionally logs a warning if not found.
      * @return true if the plugin is installed and enabled, false otherwise.
