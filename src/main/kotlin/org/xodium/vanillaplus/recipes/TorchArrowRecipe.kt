525b8b1b90d2feeefe07f5517f48ed4741d4bd64
Illyrius <fitimq@live.nl>
2025-12-03 08:13:09 +0100

    Enhance TorchArrowRecipe to utilize persistent data for arrow crafting
    Signed-off-by: Illyrius <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/recipes/TorchArrowRecipe.kt
+++ src/main/kotlin/org/xodium/vanillaplus/recipes/TorchArrowRecipe.kt
@@ -4,16 +4,21 @@ import org.bukkit.Material
 import org.bukkit.NamespacedKey
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.ShapelessRecipe
+import org.bukkit.persistence.PersistentDataType
 import org.xodium.vanillaplus.VanillaPlus.Companion.instance
 import org.xodium.vanillaplus.interfaces.RecipeInterface
 
 /** Represents an object handling torch arrow recipe implementation within the system. */
 internal object TorchArrowRecipe : RecipeInterface {
+    private val torchArrowKey = NamespacedKey(instance, "torch_arrow")
+
     override val recipes =
         setOf(
             ShapelessRecipe(
-                NamespacedKey(instance, "torch_arrow"),
-                ItemStack.of(Material.ARROW),
+                torchArrowKey,
+                ItemStack.of(Material.ARROW).apply {
+                    editPersistentDataContainer { it.set(torchArrowKey, PersistentDataType.BYTE, 1) }
+                },
             ).apply {
                 addIngredient(Material.ARROW)
                 addIngredient(Material.TORCH)
