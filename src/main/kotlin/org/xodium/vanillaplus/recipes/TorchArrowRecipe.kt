9d06e3a0a4e6015e185642306b66ec72e685161d
Illyrius <fitimq@live.nl>
2025-12-03 08:16:21 +0100

    Add custom name to Torch Arrow in recipe implementation
    Signed-off-by: Illyrius <fitimq@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/recipes/TorchArrowRecipe.kt
+++ src/main/kotlin/org/xodium/vanillaplus/recipes/TorchArrowRecipe.kt
@@ -1,5 +1,6 @@
 package org.xodium.vanillaplus.recipes
 
+import io.papermc.paper.datacomponent.DataComponentTypes
 import org.bukkit.Material
 import org.bukkit.NamespacedKey
 import org.bukkit.inventory.ItemStack
@@ -7,6 +8,7 @@ import org.bukkit.inventory.ShapelessRecipe
 import org.bukkit.persistence.PersistentDataType
 import org.xodium.vanillaplus.VanillaPlus.Companion.instance
 import org.xodium.vanillaplus.interfaces.RecipeInterface
+import org.xodium.vanillaplus.utils.ExtUtils.mm
 
 /** Represents an object handling torch arrow recipe implementation within the system. */
 internal object TorchArrowRecipe : RecipeInterface {
@@ -17,6 +19,8 @@ internal object TorchArrowRecipe : RecipeInterface {
             ShapelessRecipe(
                 torchArrowKey,
                 ItemStack.of(Material.ARROW).apply {
+                    @Suppress("UnstableApiUsage")
+                    setData(DataComponentTypes.CUSTOM_NAME, "Torch Arrow".mm())
                     editPersistentDataContainer { it.set(torchArrowKey, PersistentDataType.BYTE, 1) }
                 },
             ).apply {
