b0c3b90668facb006a12c2f97d5987042d6ca8fa
Illyrius <fitimq@live.nl>
2025-12-05 10:20:12 +0100

    v+ rc1
    Signed-off-by: Illyrius <fitimq@live.nl>

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     id("xyz.jpenilla.resource-factory-paper-convention") version "1.3.1"
 }
 
-val mcVersion = "1.21.11-pre5"
+val mcVersion = "1.21.11-rc1"
 
 group = "org.xodium.vanillaplus.VanillaPlus"
 version = mcVersion
