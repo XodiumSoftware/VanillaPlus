a4778f489ac3cc61b2e71cf68b3278681871e67b
Illyrius <FitimQ@live.nl>
2025-12-08 14:53:06 +0100

    refactor: relocate executesCatching import to CommandUtils for improved structure
    Signed-off-by: Illyrius <FitimQ@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/managers/ConfigManager.kt
+++ src/main/kotlin/org/xodium/vanillaplus/managers/ConfigManager.kt
@@ -13,7 +13,7 @@ import org.xodium.vanillaplus.VanillaPlus.Companion.instance
 import org.xodium.vanillaplus.data.CommandData
 import org.xodium.vanillaplus.data.ConfigData
 import org.xodium.vanillaplus.strategies.CapitalizedStrategy
-import org.xodium.vanillaplus.utils.ExtUtils.executesCatching
+import org.xodium.vanillaplus.utils.CommandUtils.executesCatching
 import org.xodium.vanillaplus.utils.ExtUtils.mm
 import org.xodium.vanillaplus.utils.ExtUtils.prefix
 import java.io.File
