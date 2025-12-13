71950457bbb85d49cd27a7ba6187b84d8a68959e
Illyrius <FitimQ@live.nl>
2025-12-13 20:22:34 +0100

    Refactor horde mechanics to simplify zombie spawning and targeting logic
    Signed-off-by: Illyrius <FitimQ@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/modules/HordeModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/HordeModule.kt
@@ -6,10 +6,6 @@ import org.bukkit.entity.Zombie
 import org.bukkit.event.EventHandler
 import org.bukkit.event.entity.CreatureSpawnEvent
 import org.xodium.vanillaplus.interfaces.ModuleInterface
-import java.lang.Math.random
-import kotlin.math.PI
-import kotlin.math.cos
-import kotlin.math.sin
 
 /** Represents a module handling horde mechanics within the system. */
 internal object HordeModule : ModuleInterface {
@@ -29,23 +25,15 @@ internal object HordeModule : ModuleInterface {
         }
 
         val location = event.location
-        val entityType = event.entityType
+        val world = location.world
 
         repeat(config.hordeModule.spawnModifier) {
-            val angle = random() * 2 * PI
-            val distance = random() * 2.0 + 1.0
-            val spawnLocation = location.clone().add(cos(angle) * distance, 0.0, sin(angle) * distance)
-
-            spawnLocation.y =
-                location.world
-                    .getHighestBlockAt(spawnLocation)
-                    .location.y
-
-            val zombie = location.world.spawnEntity(spawnLocation, entityType) as Zombie
+            val zombie = world.spawnEntity(location, event.entityType) as Zombie
 
             zombie.target =
-                event.entity.world.players
-                    .minByOrNull { it.location.distance(location) }
+                world
+                    .getNearbyPlayers(location, config.hordeModule.maxTargetDistance)
+                    .randomOrNull()
         }
     }
 
@@ -53,5 +41,6 @@ internal object HordeModule : ModuleInterface {
     data class Config(
         val enabled: Boolean = true,
         val spawnModifier: Int = 199,
+        val maxTargetDistance: Double = 32.0,
     )
 }
