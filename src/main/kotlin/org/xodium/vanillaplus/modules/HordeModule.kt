b51090760b6a2dfc0b8f5b9b1d0deeb0ceb0bf2a
Illyrius <FitimQ@live.nl>
2025-12-13 20:31:47 +0100

    Enhance horde mechanics by adding random scale attribute for zombies during spawning
    Signed-off-by: Illyrius <FitimQ@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/modules/HordeModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/HordeModule.kt
@@ -1,11 +1,13 @@
 package org.xodium.vanillaplus.modules
 
 import kotlinx.serialization.Serializable
+import org.bukkit.attribute.Attribute
 import org.bukkit.entity.EntityType
 import org.bukkit.entity.Zombie
 import org.bukkit.event.EventHandler
 import org.bukkit.event.entity.CreatureSpawnEvent
 import org.xodium.vanillaplus.interfaces.ModuleInterface
+import kotlin.random.Random
 
 /** Represents a module handling horde mechanics within the system. */
 internal object HordeModule : ModuleInterface {
@@ -29,7 +31,9 @@ internal object HordeModule : ModuleInterface {
 
         repeat(config.hordeModule.spawnModifier) {
             val zombie = world.spawnEntity(location, event.entityType) as Zombie
+            val scale = Random.nextDouble(config.hordeModule.scale.start, config.hordeModule.scale.endInclusive)
 
+            zombie.getAttribute(Attribute.SCALE)?.baseValue = scale
             zombie.target =
                 world
                     .getNearbyPlayers(location, config.hordeModule.maxTargetDistance)
@@ -42,5 +46,6 @@ internal object HordeModule : ModuleInterface {
         val enabled: Boolean = true,
         val spawnModifier: Int = 199,
         val maxTargetDistance: Double = 32.0,
+        val scale: ClosedRange<Double> = 0.6..1.6,
     )
 }
