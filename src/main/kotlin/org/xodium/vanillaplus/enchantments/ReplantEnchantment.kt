355f840fed022d7bb0a67aba399e436b60b7c551
Illyrius <28700752+illyrius666@users.noreply.github.com>
2026-01-13 17:50:30 +0100

    fix/ReplantEnchantment/SeedUsage (#359)
    * init
    
    Signed-off-by: Illyrius <FitimQ@live.nl>
    
    * refactor: simplify seed usage logic in ReplantEnchantment
    
    Signed-off-by: Illyrius <FitimQ@live.nl>
    
    ---------
    
    Signed-off-by: Illyrius <FitimQ@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/enchantments/ReplantEnchantment.kt
+++ src/main/kotlin/org/xodium/vanillaplus/enchantments/ReplantEnchantment.kt
@@ -33,16 +33,9 @@ internal object ReplantEnchantment : EnchantmentInterface {
         if (ageable.age < ageable.maximumAge) return
         if (!itemInHand.hasItemMeta() || !itemInHand.itemMeta.hasEnchant(get())) return
 
-        // TODO: take seed out of drop. since planting would require a seed being used.
-
         instance.server.scheduler.runTaskLater(
             instance,
-            Runnable {
-                val blockType = block.type
-
-                block.type = blockType
-                block.blockData = ageable.apply { age = 0 }
-            },
+            Runnable { block.blockData = ageable.apply { age = 0 } },
             2,
         )
     }
