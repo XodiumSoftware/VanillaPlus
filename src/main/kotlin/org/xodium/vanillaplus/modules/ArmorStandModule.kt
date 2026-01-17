4928a2f234ff8a5c739c7f19528048c94be03654
Illyrius <FitimQ@live.nl>
2026-01-17 15:47:23 +0100

    feat: implement armor stand interaction handling
    Signed-off-by: Illyrius <FitimQ@live.nl>

--- src/main/kotlin/org/xodium/vanillaplus/modules/ArmorStandModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/ArmorStandModule.kt
@@ -1,10 +1,33 @@
 package org.xodium.vanillaplus.modules
 
 import kotlinx.serialization.Serializable
+import org.bukkit.entity.ArmorStand
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.block.Action
+import org.bukkit.event.entity.EntityInteractEvent
 import org.xodium.vanillaplus.interfaces.ModuleInterface
+import org.xodium.vanillaplus.menus.ArmorStandMenu.menu
 
 /** Represents a module handling armor stand mechanics within the system. */
 internal object ArmorStandModule : ModuleInterface {
+    @EventHandler
+    fun on(event: EntityInteractEvent) = handleArmorStandInventory(event)
+
+    /**
+     * Handles the interaction with an ArmorStand's inventory.
+     * @param event EntityInteractEvent The event triggered by the interaction.
+     */
+    private fun handleArmorStandInventory(event: EntityInteractEvent) {
+        val armorStand = event.entity as? ArmorStand ?: return
+        val player = event.entity as? Player ?: return
+
+        if (player.isSneaking && Action.RIGHT_CLICK_BLOCK.equals(armorStand)) {
+            event.isCancelled = true
+            player.openInventory(armorStand.menu(player))
+        }
+    }
+
     /** Represents the config of the module. */
     @Serializable
     data class Config(
