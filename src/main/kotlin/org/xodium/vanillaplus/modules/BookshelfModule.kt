2ef704cfdeb5eb01dce9ec03dc50c6427f653d4f
illyrius666 <28700752+illyrius666@users.noreply.github.com>
2026-02-27 10:51:17 +0100

    impl
    Signed-off-by: illyrius666 <28700752+illyrius666@users.noreply.github.com>

--- src/main/kotlin/org/xodium/vanillaplus/modules/BookshelfModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/BookshelfModule.kt
@@ -3,14 +3,32 @@ package org.xodium.vanillaplus.modules
 import kotlinx.serialization.Serializable
 import org.bukkit.block.ChiseledBookshelf
 import org.bukkit.event.EventHandler
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryDragEvent
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.ChiseledBookshelfInventory
 import org.xodium.vanillaplus.interfaces.ModuleInterface
 
 /** Represents a module handling bookshelf mechanics within the system. */
 internal object BookshelfModule : ModuleInterface {
     @EventHandler
     fun on(event: PlayerInteractEvent) {
-        (event.clickedBlock?.state as? ChiseledBookshelf)?.inventory?.let(event.player::openInventory)
+        if (!event.player.isSneaking) return
+
+        (event.clickedBlock?.state as? ChiseledBookshelf)?.snapshotInventory?.let {
+            event.isCancelled = true
+            event.player.openInventory(it)
+        }
+    }
+
+    @EventHandler
+    fun on(event: InventoryClickEvent) {
+        if (event.inventory is ChiseledBookshelfInventory) event.isCancelled = true
+    }
+
+    @EventHandler
+    fun on(event: InventoryDragEvent) {
+        if (event.inventory is ChiseledBookshelfInventory) event.isCancelled = true
     }
 
     /** Represents the config of the module. */
