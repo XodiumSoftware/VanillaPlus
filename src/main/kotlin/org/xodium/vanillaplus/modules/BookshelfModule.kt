8d1e9a40537e65b0c51b1bf98c853772ff389c7a
illyrius666 <28700752+illyrius666@users.noreply.github.com>
2026-02-27 12:03:15 +0100

    +
    Signed-off-by: illyrius666 <28700752+illyrius666@users.noreply.github.com>

--- src/main/kotlin/org/xodium/vanillaplus/modules/BookshelfModule.kt
+++ src/main/kotlin/org/xodium/vanillaplus/modules/BookshelfModule.kt
@@ -1,6 +1,7 @@
 package org.xodium.vanillaplus.modules
 
 import kotlinx.serialization.Serializable
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder
 import org.bukkit.block.ChiseledBookshelf
 import org.bukkit.event.EventHandler
 import org.bukkit.event.player.PlayerInteractEvent
@@ -21,75 +22,84 @@ internal object BookshelfModule : ModuleInterface {
 
         event.isCancelled = true
 
-        val inventory = bookshelf.snapshotInventory
+        val inventory = bookshelf.inventory
 
-        player.sendMessage(MM.deserialize("<gradient:#FFA751:#FFE259><b>Chiseled Bookshelf Content:</b></gradient>"))
+        player.sendMessage(MM.deserialize(config.bookshelfModule.header))
 
         var hasItems = false
 
         for (i in 0 until inventory.size) {
-            val item = inventory.getItem(i) ?: continue
+            inventory.getItem(i) ?: continue
 
             hasItems = true
+            break
+        }
 
+        if (!hasItems) return
+
+        for (i in 0 until inventory.size) {
+            val item = inventory.getItem(i) ?: continue
             val slotNumber = i + 1
+            val slotPrefix =
+                MM.deserialize(
+                    config.bookshelfModule.slotPrefix,
+                    Placeholder.component("slot", MM.deserialize(slotNumber.toString())),
+                )
 
             when (val itemMeta = item.itemMeta) {
                 is BookMeta -> {
-                    val title = itemMeta.title ?: "Untitled"
+                    val title =
+                        itemMeta.title ?: item.type.name
+                            .lowercase()
+                            .replace('_', ' ')
                     val author = itemMeta.author
 
                     if (author != null) {
                         player.sendMessage(
-                            MM.deserialize(
-                                "<yellow>$slotNumber.</yellow> <white>$title</white><gray> by $author</gray>",
+                            slotPrefix.append(
+                                MM.deserialize(
+                                    " <white><sprite:items:item/written_book></white> <white>$title</white><gray> by $author</gray>",
+                                ),
                             ),
                         )
                     } else {
                         player.sendMessage(
-                            MM.deserialize("<yellow>$slotNumber.</yellow> <white>$title</white>"),
+                            slotPrefix.append(
+                                MM.deserialize(" <white><sprite:items:item/book></white>"),
+                            ),
                         )
                     }
                 }
 
                 is EnchantmentStorageMeta -> {
                     val enchantments = itemMeta.storedEnchants
+                    val enchantmentList = enchantments.entries.joinToString(", ") { "${it.key.key.key} ${it.value}" }
 
-                    if (enchantments.isEmpty()) {
-                        player.sendMessage(
-                            MM.deserialize("<yellow>$slotNumber.</yellow> <white>Empty Enchanted Book</white>"),
-                        )
-                    } else {
-                        val enchantmentList =
-                            enchantments.entries.joinToString(", ") {
-                                "${it.key.key.key} ${it.value}"
-                            }
-
-                        player.sendMessage(
+                    player.sendMessage(
+                        slotPrefix.append(
                             MM.deserialize(
-                                "<yellow>$slotNumber.</yellow> <white>Enchanted Book</white><gray> | $enchantmentList</gray>",
+                                " <white><sprite:items:item/enchanted_book></white><gray> > $enchantmentList</gray>",
                             ),
-                        )
-                    }
+                        ),
+                    )
                 }
 
                 else -> {
-                    val itemName =
-                        item.type.name
-                            .lowercase()
-                            .replace('_', ' ')
+                    val itemName = item.type.key.key
 
-                    player.sendMessage(MM.deserialize("<yellow>$slotNumber.</yellow> <white>$itemName</white>"))
+                    player.sendMessage(
+                        slotPrefix.append(MM.deserialize(" <white><sprite:items:item/$itemName></white>")),
+                    )
                 }
             }
         }
-
-        if (!hasItems) player.sendMessage(MM.deserialize("<gray>Empty</gray>"))
     }
 
     /** Represents the config of the module. */
     @Serializable
     data class Config(
         var enabled: Boolean = true,
+        var header: String = "<gradient:#FFA751:#FFE259><b>Chiseled Bookshelf Content:</b></gradient>",
+        var slotPrefix: String = "<gradient:#CB2D3E:#EF473A><slot>.</gradient>",
     )
 }
