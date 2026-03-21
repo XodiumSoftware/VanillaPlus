9547e03db3caf1e8c6b70fa0300d6d05a03c49ac
Illyrius <FitimQ@live.nl>
2026-03-21 17:45:15 +0100

    removed rules book
    Signed-off-by: Illyrius <FitimQ@live.nl>

--- src/modules/ServerInfoModule.kt
+++ src/modules/ServerInfoModule.kt
@@ -3,7 +3,6 @@ package org.xodium.vanillaplus.modules
 import kotlinx.serialization.Serializable
 import org.bukkit.ServerLinks
 import org.xodium.vanillaplus.VanillaPlus.Companion.instance
-import org.xodium.vanillaplus.data.BookData
 import org.xodium.vanillaplus.interfaces.ModuleConfigInterface
 import org.xodium.vanillaplus.interfaces.ModuleInterface
 import org.xodium.vanillaplus.utils.Utils.configDelegate
@@ -35,46 +34,5 @@ internal object ServerInfoModule : ModuleInterface {
                 ServerLinks.Type.STATUS to "https://modrinth.com/server/illyria",
                 ServerLinks.Type.COMMUNITY to "https://discord.gg/jusYH9aYUh",
             ),
-        var books: Books = Books(),
-    ) : ModuleConfigInterface {
-        @Serializable
-        data class Books(
-            var rules: BookData =
-                BookData(
-                    title = "<fire>Rules</fire>",
-                    pages =
-                        listOf(
-                            // Page 1: Player Rules (1-7)
-                            listOf(
-                                "<b><u><dark_aqua>Player Rules:<reset>",
-                                "",
-                                "<gold>▶ <dark_aqua>01 <dark_gray>| <red>No Griefing",
-                                "<gold>▶ <dark_aqua>02 <dark_gray>| <red>No Spamming",
-                                "<gold>▶ <dark_aqua>03 <dark_gray>| <red>No Advertising",
-                                "<gold>▶ <dark_aqua>04 <dark_gray>| <red>No Cursing/No Constant Cursing",
-                                "<gold>▶ <dark_aqua>05 <dark_gray>| <red>No Trolling/Flaming",
-                                "<gold>▶ <dark_aqua>06 <dark_gray>| <red>No Asking for OP, Ranks, or Items",
-                                "<gold>▶ <dark_aqua>07 <dark_gray>| <red>Respect all Players",
-                            ),
-                            // Page 2: Player Rules (8-13)
-                            listOf(
-                                "<gold>▶ <dark_aqua>08 <dark_gray>| <red>Obey Staff they are the Law Enforcers",
-                                "<gold>▶ <dark_aqua>09 <dark_gray>| <red>No Racist or Sexist Remarks",
-                                "<gold>▶ <dark_aqua>10 <dark_gray>| <red>No Mods/Hacks",
-                                "<gold>▶ <dark_aqua>12 <dark_gray>| <red>No 1x1 Towers",
-                                "<gold>▶ <dark_aqua>13 <dark_gray>| <red>Build in (Fantasy)Medieval style",
-                            ),
-                            // Page 3: Mod/Admin Rules
-                            listOf(
-                                "<b><u><dark_aqua>Mod/Admin Rules:<reset>",
-                                "",
-                                "<gold>▶ <dark_aqua>01 <dark_gray>| <red>Be Responsible with the power you are given as staff",
-                                "<gold>▶ <dark_aqua>02 <dark_gray>| <red>Do not spawn blocks or items for other players",
-                                "<gold>▶ <dark_aqua>03 <dark_gray>| <red>When Trading, only buy and sell legit items",
-                                "<gold>▶ <dark_aqua>05 <dark_gray>| <red>No Power Abuse",
-                            ),
-                        ),
-                ),
-        )
-    }
+    ) : ModuleConfigInterface
 }
